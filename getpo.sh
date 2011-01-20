2512ce7145b6f1fc4158ab8f756670cf90443698
Ciaran Gultnieks <ciaran@ciarang.com>
2011-01-20 23:34:29 +0000

    Improvement to the getpo script

--- getpo.sh
+++ getpo.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+ssh fdroid@f-droid.org -C "/home/fdroid/public_html/translate/manage.py sync_stores"
 scp fdroid@f-droid.org:/home/fdroid/public_html/translate/po/fdroidclient/*.po locale
 a2po import
