0d9fb6ea4dbd785e5ea0f89863b369dac0423362
Ciaran Gultnieks <ciaran@ciarang.com>
2011-10-11 14:06:00 +0100

    Translation updates

--- putpo.sh
+++ putpo.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 a2po export
 scp locale/*.po* fdroid@f-droid.org:/home/fdroid/public_html/translate/po/fdroidclient/
+ssh fdroid@f-droid.org -C "/home/fdroid/public_html/translate/manage.py update_stores"
