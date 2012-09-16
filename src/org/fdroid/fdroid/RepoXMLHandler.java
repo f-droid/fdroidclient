121d5cdd2b2dbbf3a79ab4355a58e506ae81abb3
Ciaran Gultnieks <ciaran@ciarang.com>
2012-09-16 10:29:24 +0100

    Removes unused import

--- src/org/fdroid/fdroid/RepoXMLHandler.java
+++ src/org/fdroid/fdroid/RepoXMLHandler.java
@@ -28,7 +28,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.security.cert.Certificate;
