4489037619d4d75a475ab3698eaf810507593e40
Hans-Christoph Steiner <hans@eds.org>
2014-01-31 21:49:13 -0500

    NFC beam the repo in RepoDetailsActivity
    This is the framework for easily swapping repos.  The idea is that a user
    can send the URL with the fingerprint for trusted bootstrapping of the repo
    on a new user's device.  This will be essential for p2p repos provided
    by Bazaar/Kerplapp.
    
    The required NFC APIs were introduced in android-14. So android-14 and below
    skip the NFC stuff.

--- src/org/fdroid/fdroid/views/RepoDetailsActivity.java
+++ src/org/fdroid/fdroid/views/RepoDetailsActivity.java
@@ -1,12 +1,21 @@
 
 package org.fdroid.fdroid.views;
 
+import android.annotation.TargetApi;
+import android.content.Intent;
 import android.net.Uri;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.v4.app.FragmentActivity;
 import android.text.TextUtils;
+import android.util.Log;
+import android.widget.Toast;
 
 import org.fdroid.fdroid.FDroidApp;
 import org.fdroid.fdroid.compat.ActionBarCompat;
@@ -15,9 +24,12 @@ import org.fdroid.fdroid.data.RepoProvider;
 import org.fdroid.fdroid.views.fragments.RepoDetailsFragment;
 
 public class RepoDetailsActivity extends FragmentActivity {
+    public static final String TAG = "RepoDetailsActivity";
 
     private WifiManager wifiManager;
     private Repo repo;
+
+    static final String MIME_TYPE = "application/vnd.org.fdroid.fdroid.repo";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,9 +44,9 @@ public class RepoDetailsActivity extends FragmentActivity {
             RepoDetailsFragment fragment = new RepoDetailsFragment();
             fragment.setArguments(getIntent().getExtras());
             getSupportFragmentManager()
-                .beginTransaction()
-                .add(android.R.id.content, fragment)
-                .commit();
+                    .beginTransaction()
+                    .add(android.R.id.content, fragment)
+                    .commit();
         }
 
         String[] projection = new String[] {
@@ -48,6 +60,63 @@ public class RepoDetailsActivity extends FragmentActivity {
         setTitle(repo.getName());
 
         wifiManager = (WifiManager) getSystemService(WIFI_SERVICE);
+
+        // required NFC support starts in android-14
+        if (Build.VERSION.SDK_INT >= 14)
+            setNfc();
+    }
+
+    @TargetApi(14)
+    private void setNfc() {
+        NfcAdapter nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if (nfcAdapter == null) {
+            return;
+        }
+        nfcAdapter.setNdefPushMessage(new NdefMessage(new NdefRecord[] {
+                NdefRecord.createUri(getSharingUri()),
+        }), this);
+        findViewById(android.R.id.content).post(new Runnable() {
+            @Override
+            public void run() {
+                Log.i(TAG, "Runnable.run()");
+                onNewIntent(getIntent());
+            }
+        });
+    }
+
+    @Override
+    public void onResume() {
+        Log.i(TAG, "onResume");
+        super.onResume();
+        if (Build.VERSION.SDK_INT >= 9)
+            processIntent(getIntent());
+    }
+
+    @Override
+    public void onNewIntent(Intent i) {
+        Log.i(TAG, "onNewIntent");
+        Log.i(TAG, "action: " + i.getAction());
+        Log.i(TAG, "data: " + i.getData());
+        // onResume gets called after this to handle the intent
+        setIntent(i);
+    }
+
+    @TargetApi(9)
+    void processIntent(Intent i) {
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(i.getAction())) {
+            Log.i(TAG, "ACTION_NDEF_DISCOVERED");
+            Parcelable[] rawMsgs =
+                    i.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            NdefMessage msg = (NdefMessage) rawMsgs[0];
+            String url = new String(msg.getRecords()[0].getPayload());
+            Log.i(TAG, "Got this URL: " + url);
+            Toast.makeText(this, "Got this URL: " + url, Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            String packageName = getPackageName();
+            intent.setClassName(packageName, packageName + ".ManageRepo");
+            startActivity(intent);
+            finish();
+        }
     }
 
     protected Uri getSharingUri() {
