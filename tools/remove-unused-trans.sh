5292acfef0b843d357bf12554837c685b48ee150
Daniel Martí <mvdan@mvdan.cc>
2014-02-10 09:22:43 +0100

    Add support for arrays in remove-unused-trans

--- tools/remove-unused-trans.sh
+++ tools/remove-unused-trans.sh
@@ -3,7 +3,11 @@
 # Remove extra translations
 
 lint . --quiet --check ExtraTranslation --nolines | \
-	sed -n 's/.*Error: "\([^"]*\)" is translated here but not found in default locale.*/\1/p' | \
-	while read name; do
-		sed -i "/name=\"$name\"/d" res/values-*/strings.xml
+	sed -n 's@res/values-[^/]\+/\([^\.]\+\)\.xml:.*Error: "\([^"]*\)" is translated here but not found in default locale.*@\1 \2@p' | \
+	while read file name; do
+		if [[ $file == strings ]]; then
+			sed -i "/name=\"$name\"/d" res/values-*/strings.xml
+		elif [[ $file == array ]]; then
+			sed -i "/<string-array name=\"$name\"/,/<\/string-array/d" res/values-*/array.xml
+		fi
 	done
