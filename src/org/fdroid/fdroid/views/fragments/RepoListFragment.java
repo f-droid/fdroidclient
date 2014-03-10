49a3c3370f35b6104e53ab27a3eb1df4ebc94f70
Daniel Martí <mvdan@mvdan.cc>
2014-03-10 18:36:35 +0100

    Don't finish the whole Repositores activity when cancelling "New Repository"

--- src/org/fdroid/fdroid/views/fragments/RepoListFragment.java
+++ src/org/fdroid/fdroid/views/fragments/RepoListFragment.java
@@ -408,9 +408,7 @@ public class RepoListFragment extends ListFragment
                 new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        getActivity().setResult(Activity.RESULT_CANCELED);
-                        getActivity().finish();
-                        return;
+                        dialog.dismiss();
                     }
                 });
         alrt.show();
