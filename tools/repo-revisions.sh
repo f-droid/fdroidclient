edd2de49c317ecd59ab7c9c193c7a3b7cc6edc3d
Daniel Martí <mvdan@mvdan.cc>
2014-04-01 15:16:24 +0200

    Use HEAD instead of LAST_STABLE_TAG

--- tools/repo-revisions.sh
+++ tools/repo-revisions.sh
@@ -2,9 +2,9 @@
 
 # Update README repo manifest revisions from git
 
-LAST_STABLE_TAG=$(git describe --abbrev=0 --tags --match='*[0-9]')
-sed -i 's@\(.*name="fdroidclient\.git".*revision="\)[^"]*\(".*\)@\1'$LAST_STABLE_TAG'\2@' README.md
+#LAST_STABLE_TAG=$(git describe --abbrev=0 --tags --match='[0-9]*[0-9]')
+#sed -i 's@\(.*name="fdroidclient\.git".*revision="\)[^"]*\(".*\)@\1'$LAST_STABLE_TAG'\2@' README.md
 
-git ls-tree $LAST_STABLE_TAG extern/ | while read _ _ revision path; do
+git ls-tree HEAD extern/ | while read _ _ revision path; do
 	sed -i 's@\(.*fdroidclient/'$path'".*revision="\)[^"]*\(".*\)@\1'$revision'\2@' README.md
 done
