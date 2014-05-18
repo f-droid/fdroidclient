aa52516a41dbd206aa286c702639e8d487b24254
Daniel Martí <mvdan@mvdan.cc>
2014-05-18 13:44:44 +0200

    Forgot to rollback the ant-prepare nanohttp path

--- ant-prepare.sh
+++ ant-prepare.sh
@@ -3,7 +3,8 @@
 android update lib-project --path extern/UniversalImageLoader/library
 android update lib-project --path extern/AndroidPinning
 android update lib-project --path extern/MemorizingTrustManager
-android update lib-project --path extern/nanohttpd/core
+android update lib-project --path extern/nanohttpd
+#android update lib-project --path extern/nanohttpd/core
 android update lib-project --path extern/libsuperuser/libsuperuser
 android update project --path . --name F-Droid
 
