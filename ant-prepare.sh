b7feebddf95d09adba72a33c4ffb78816ca609ca
Daniel Martí <mvdan@mvdan.cc>
2014-05-20 18:48:38 +0200

    Also use jmdns like a java library in ant

--- ant-prepare.sh
+++ ant-prepare.sh
@@ -3,9 +3,7 @@
 android update lib-project --path extern/UniversalImageLoader/library
 android update lib-project --path extern/AndroidPinning
 android update lib-project --path extern/MemorizingTrustManager
-android update lib-project --path extern/nanohttpd/core
 android update lib-project --path extern/libsuperuser/libsuperuser
-android update lib-project --path extern/jmdns
 android update project --path . --name F-Droid
 
 { echo -e "\nSuccessfully updated the main project.\n"; } 2>/dev/null
