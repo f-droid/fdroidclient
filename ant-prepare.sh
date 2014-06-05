81cbef82b4e58b115363c5c2bfb207a3ba9dabc2
Hans-Christoph Steiner <hans@eds.org>
2014-06-04 23:12:49 -0400

    finish ant/gradle build for appcompat-v7
    Both preferencefragment and appcompat-v7 submodules need the
    android-support-v4.jar to be included in their respective libs/ dirs in
    order for ant to build those projects.

--- ant-prepare.sh
+++ ant-prepare.sh
@@ -1,10 +1,14 @@
 #!/bin/bash -ex
 
+target=`sed -n 's,^target=\(.*\),\1,p' project.properties`
+
 android update lib-project --path extern/UniversalImageLoader/library
 android update lib-project --path extern/AndroidPinning
 android update lib-project --path extern/MemorizingTrustManager
 android update lib-project --path extern/libsuperuser/libsuperuser
 android update lib-project --path extern/zxing-core
+android update lib-project --path extern/android-support-v4-preferencefragment
+android update lib-project --path extern/Support/v7/appcompat --target $target
 android update project --path . --name F-Droid
 
 { echo -e "\nSuccessfully updated the main project.\n"; } 2>/dev/null
