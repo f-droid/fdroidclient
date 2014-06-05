ce427835830d61f4d0e9a9c5ec83b5570d818f24
Daniel Martí <mvdan@mvdan.cc>
2014-06-05 10:15:26 +0200

    Move support-v4 jars around in ant-prepare

--- ant-prepare.sh
+++ ant-prepare.sh
@@ -1,12 +1,26 @@
 #!/bin/bash -ex
 
+place_support_v4() {
+    mkdir -p $1/libs
+    cp libs/android-support-v4.jar $1/libs/
+}
+
 android update lib-project --path extern/UniversalImageLoader/library
+
 android update lib-project --path extern/AndroidPinning
+
 android update lib-project --path extern/MemorizingTrustManager
+
 android update lib-project --path extern/libsuperuser/libsuperuser
+
 android update lib-project --path extern/zxing-core
+
 android update lib-project --path extern/android-support-v4-preferencefragment
+place_support_v4 extern/android-support-v4-preferencefragment
+
 android update lib-project --path extern/Support/v7/appcompat --target android-19
+place_support_v4 extern/Support/v7/appcompat
+
 android update project --path . --name F-Droid
 
 { echo -e "\nSuccessfully updated the main project.\n"; } 2>/dev/null
