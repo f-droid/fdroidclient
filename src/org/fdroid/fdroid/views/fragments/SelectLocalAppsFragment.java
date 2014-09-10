f-droid/fdroidclient

commit 4f7f87be0a0487aad19c78a2a44faa1c2f008361

assign static var FDroidApp.selectedApps only after HashSet is built
This should help is there is ever multithreaded access to this variable.
This is an unlikely scenario, but the fix is easy.

findbugs reported this issue like this:

Incorrect lazy initialization and update of static field org.fdroid.fdroid.
FDroidApp.selectedApps in org.fdroid.fdroid.views.fragments.
SelectLocalAppsFragment.onActivityCreated(Bundle)

This method contains an unsynchronized lazy initialization of a static
field. After the field is set, the object stored into that location is
further updated or accessed. The setting of the field is visible to other
threads as soon as it is set. If the futher accesses in the method that set
the field serve to initialize the object, then you have a very serious
multithreading bug, unless something else prevents any other thread from
accessing the stored object until it is fully initialized.

src/org/fdroid/fdroid/views/fragments/SelectLocalAppsFragment.java
@@ -112,13 +112,14 @@ public class SelectLocalAppsFragment extends ListFragment
 
         // build list of existing apps from what is on the file system
         if (FDroidApp.selectedApps == null) {
-            FDroidApp.selectedApps = new HashSet<String>();
+            HashSet<String> selectedApps = new HashSet<String>();
             for (String filename : LocalRepoManager.get(selectLocalAppsActivity).repoDir.list()) {
                 if (filename.matches(".*\\.apk")) {
                     String packageName = filename.substring(0, filename.indexOf("_"));
                     FDroidApp.selectedApps.add(packageName);
                 }
             }
+            FDroidApp.selectedApps = selectedApps;
         }
     }
 

commit 008752ae7443023a4294caa8a73dd5597a931e63

fix stupid crasher bug introduced in 4f7f87be0a0487aad19c78a2a44faa1c2f008361

src/org/fdroid/fdroid/views/fragments/SelectLocalAppsFragment.java
@@ -116,7 +116,7 @@ public class SelectLocalAppsFragment extends ListFragment
             for (String filename : LocalRepoManager.get(selectLocalAppsActivity).repoDir.list()) {
                 if (filename.matches(".*\\.apk")) {
                     String packageName = filename.substring(0, filename.indexOf("_"));
-                    FDroidApp.selectedApps.add(packageName);
+                    selectedApps.add(packageName);
                 }
             }
             FDroidApp.selectedApps = selectedApps;