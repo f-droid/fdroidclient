f-droid/fdroidclient

commit e4401ed22c0f65db5d4f1a1f0e1222b061e471af

Don't show progress for every app being processed from index.xml.
Rather, only show 25 progress events. I went with "25 events" rather
than "every X apps" because then it will be nice for both large repos
(e.g. F-Droid will update every ~50 apps) and small repos (something
with 20 apps will update for every one, but not add much overhead).

On my testing with an API 11 emulator here, it went from ~32s to
process ~1100 apps to ~20s. When no progress events are sent, then it
also takes ~20s, so this essentially is a 50% improvement for this part
of the update process.

src/org/fdroid/fdroid/RepoXMLHandler.java
@@ -279,14 +279,15 @@ public class RepoXMLHandler extends DefaultHandler {
         } else if (localName.equals("application") && curapp == null) {
             curapp = new App();
             curapp.id = attributes.getValue("", "id");
+            if (progressCounter % (totalAppCount / 25) == 0) {
+                Bundle data = new Bundle(1);
+                data.putString(RepoUpdater.PROGRESS_DATA_REPO_ADDRESS, repo.address);
+                progressListener.onProgress(
+                    new ProgressListener.Event(
+                        RepoUpdater.PROGRESS_TYPE_PROCESS_XML,
+                        progressCounter, totalAppCount, data));
+            }
             progressCounter ++;
-            Bundle data = new Bundle(1);
-            data.putString(RepoUpdater.PROGRESS_DATA_REPO_ADDRESS, repo.address);
-            progressListener.onProgress(
-                new ProgressListener.Event(
-                    RepoUpdater.PROGRESS_TYPE_PROCESS_XML,
-                    progressCounter, totalAppCount, data));
-
         } else if (localName.equals("package") && curapp != null && curapk == null) {
             curapk = new Apk();
             curapk.id = curapp.id;

commit 47d6bb095ba5bfb119c7db6d6f6bdc326e629426

fix divide-by-zero crash when a repo has less than 25 apps in it
This was introduced in e4401ed22c0f65db5d4f1a1f0e1222b061e471af

src/org/fdroid/fdroid/RepoXMLHandler.java
@@ -20,6 +20,7 @@
 package org.fdroid.fdroid;
 
 import android.os.Bundle;
+
 import org.fdroid.fdroid.data.Apk;
 import org.fdroid.fdroid.data.App;
 import org.fdroid.fdroid.data.Repo;
@@ -29,7 +30,8 @@ import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
 import java.text.ParseException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RepoXMLHandler extends DefaultHandler {
 
@@ -279,7 +281,8 @@ public class RepoXMLHandler extends DefaultHandler {
         } else if (localName.equals("application") && curapp == null) {
             curapp = new App();
             curapp.id = attributes.getValue("", "id");
-            if (progressCounter % (totalAppCount / 25) == 0) {
+            /* show progress for the first 25, then start skipping every 25 */
+            if (totalAppCount < 25 || progressCounter % (totalAppCount / 25) == 0) {
                 Bundle data = new Bundle(1);
                 data.putString(RepoUpdater.PROGRESS_DATA_REPO_ADDRESS, repo.address);
                 progressListener.onProgress(