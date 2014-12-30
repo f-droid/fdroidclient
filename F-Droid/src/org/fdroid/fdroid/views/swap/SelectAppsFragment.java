23ed692436e8b27d533c6b611ebec3c9f5801a13
Peter Serwylo <peter@serwylo.com>
2014-12-31 00:27:16 +1100

    "Select apps for swap" screen work on API <= 10.
     * Provide CheckBox for selected items
    
    Newer API's highlight the background using the "activated" state. Older
    APIs need this to be implemented differently, so there are now checkboxes
    on the left of the list view items to provide this functionality.
    
     * Clean up IDE warnings
    
    Diamond operator for generics, remove unused imports and unused method.
    
     * Adapter class created for installed apps
    
    Cleaned up the code to do with binding views to the adapter in this view.
    Previously it made quite a few assumptions about the structure of the layout,
    e.g. "layout.getParent().getParent() is a LinearLayout", which would cause
    crashes if the layout changed slightly.

--- F-Droid/src/org/fdroid/fdroid/views/swap/SelectAppsFragment.java
+++ F-Droid/src/org/fdroid/fdroid/views/swap/SelectAppsFragment.java
@@ -1,27 +1,23 @@
 package org.fdroid.fdroid.views.swap;
 
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v4.view.MenuItemCompat;
+import android.support.v4.widget.CursorAdapter;
 import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.widget.SearchView;
 import android.text.TextUtils;
-import android.view.ActionMode;
-import android.view.ContextThemeWrapper;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.view.*;
+import android.widget.*;
 import org.fdroid.fdroid.FDroidApp;
 import org.fdroid.fdroid.R;
 import org.fdroid.fdroid.data.InstalledAppProvider;
@@ -34,10 +30,8 @@ import java.util.Set;
 public class SelectAppsFragment extends ThemeableListFragment
     implements LoaderManager.LoaderCallbacks<Cursor>, SearchView.OnQueryTextListener {
 
-    private PackageManager packageManager;
-    private Drawable defaultAppIcon;
     private String mCurrentFilterString;
-    private Set<String> previouslySelectedApps = new HashSet<String>();
+    private Set<String> previouslySelectedApps = new HashSet<>();
 
     public Set<String> getSelectedApps() {
         return FDroidApp.selectedApps;
@@ -103,46 +97,10 @@ public class SelectAppsFragment extends ThemeableListFragment
 
         setEmptyText(getString(R.string.no_applications_found));
 
-        packageManager = getActivity().getPackageManager();
-        defaultAppIcon = getResources().getDrawable(android.R.drawable.sym_def_app_icon);
-
         ListView listView = getListView();
         listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
-        SimpleCursorAdapter adapter = new SimpleCursorAdapter(
-                new ContextThemeWrapper(getActivity(), R.style.SwapTheme_AppList_ListItem),
-                R.layout.select_local_apps_list_item,
-                null,
-                new String[] {
-                        InstalledAppProvider.DataColumns.APPLICATION_LABEL,
-                        InstalledAppProvider.DataColumns.APP_ID,
-                },
-                new int[] {
-                        R.id.application_label,
-                        R.id.package_name,
-                });
-        adapter.setViewBinder(new SimpleCursorAdapter.ViewBinder() {
 
-            @Override
-            public boolean setViewValue(View view, Cursor cursor, int columnIndex) {
-                if (columnIndex == cursor.getColumnIndex(InstalledAppProvider.DataColumns.APP_ID)) {
-                    String packageName = cursor.getString(columnIndex);
-                    TextView textView = (TextView) view.findViewById(R.id.package_name);
-                    textView.setText(packageName);
-                    LinearLayout ll = (LinearLayout) view.getParent().getParent();
-                    ImageView iconView = (ImageView) ll.getChildAt(0);
-                    Drawable icon;
-                    try {
-                        icon = packageManager.getApplicationIcon(packageName);
-                    } catch (PackageManager.NameNotFoundException e) {
-                        icon = defaultAppIcon;
-                    }
-                    iconView.setImageDrawable(icon);
-                    return true;
-                }
-                return false;
-            }
-        });
-        setListAdapter(adapter);
+        setListAdapter(new AppListAdapter(listView, getActivity(), null));
         setListShown(false); // start out with a progress indicator
 
         // either reconnect with an existing loader or start a new one
@@ -150,7 +108,7 @@ public class SelectAppsFragment extends ThemeableListFragment
 
         // build list of existing apps from what is on the file system
         if (FDroidApp.selectedApps == null) {
-            FDroidApp.selectedApps = new HashSet<String>();
+            FDroidApp.selectedApps = new HashSet<>();
             for (String filename : LocalRepoManager.get(getActivity()).repoDir.list()) {
                 if (filename.matches(".*\\.apk")) {
                     String packageName = filename.substring(0, filename.indexOf("_"));
@@ -190,7 +148,7 @@ public class SelectAppsFragment extends ThemeableListFragment
 
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
-        ((SimpleCursorAdapter) this.getListAdapter()).swapCursor(cursor);
+        ((AppListAdapter)getListAdapter()).swapCursor(cursor);
 
         ListView listView = getListView();
         String fdroid = loader.getContext().getPackageName();
@@ -217,7 +175,7 @@ public class SelectAppsFragment extends ThemeableListFragment
 
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
-        ((SimpleCursorAdapter) this.getListAdapter()).swapCursor(null);
+        ((AppListAdapter)getListAdapter()).swapCursor(null);
     }
 
     @Override
@@ -240,10 +198,6 @@ public class SelectAppsFragment extends ThemeableListFragment
         return true;
     }
 
-    public String getCurrentFilterString() {
-        return mCurrentFilterString;
-    }
-
     @Override
     protected int getThemeStyle() {
         return R.style.SwapTheme_StartSwap;
@@ -253,4 +207,85 @@ public class SelectAppsFragment extends ThemeableListFragment
     protected int getHeaderLayout() {
         return R.layout.swap_create_header;
     }
+
+    private static class AppListAdapter extends CursorAdapter {
+
+        @Nullable
+        private LayoutInflater inflater;
+
+        @Nullable
+        private Drawable defaultAppIcon;
+
+        @NonNull
+        private final ListView listView;
+
+        public AppListAdapter(@NonNull ListView listView, @NonNull Context context, @Nullable Cursor c) {
+            super(context, c, FLAG_REGISTER_CONTENT_OBSERVER);
+            this.listView = listView;
+        }
+
+        @NonNull
+        private LayoutInflater getInflater(Context context) {
+            if (inflater == null) {
+                Context themedContext = new ContextThemeWrapper(context, R.style.SwapTheme_AppList_ListItem);
+                inflater = (LayoutInflater)themedContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            }
+            return inflater;
+        }
+
+        @NonNull
+        private Drawable getDefaultAppIcon(Context context) {
+            if (defaultAppIcon == null) {
+                defaultAppIcon = context.getResources().getDrawable(android.R.drawable.sym_def_app_icon);
+            }
+            return defaultAppIcon;
+        }
+
+        @Override
+        public View newView(Context context, Cursor cursor, ViewGroup parent) {
+            View view = getInflater(context).inflate(R.layout.select_local_apps_list_item, null);
+            bindView(view, context, cursor);
+            return view;
+        }
+
+        @Override
+        public void bindView(final View view, final Context context, final Cursor cursor) {
+
+            TextView packageView = (TextView)view.findViewById(R.id.package_name);
+            TextView labelView = (TextView)view.findViewById(R.id.application_label);
+            ImageView iconView = (ImageView)view.findViewById(android.R.id.icon);
+
+            String packageName = cursor.getString(cursor.getColumnIndex(InstalledAppProvider.DataColumns.APP_ID));
+            String appLabel = cursor.getString(cursor.getColumnIndex(InstalledAppProvider.DataColumns.APPLICATION_LABEL));
+
+            Drawable icon;
+            try {
+                icon = context.getPackageManager().getApplicationIcon(packageName);
+            } catch (PackageManager.NameNotFoundException e) {
+                icon = getDefaultAppIcon(context);
+            }
+
+            packageView.setText(packageName);
+            labelView.setText(appLabel);
+            iconView.setImageDrawable(icon);
+
+            // Since v11, the Android SDK provided the ability to show selected list items
+            // by highlighting their background. Prior to this, we need to handle this ourselves
+            // by adding a checkbox which can toggle selected items.
+            View checkBoxView = view.findViewById(R.id.checkbox);
+            if (checkBoxView != null) {
+                CheckBox checkBox = (CheckBox)checkBoxView;
+                checkBox.setOnCheckedChangeListener(null);
+                checkBox.setChecked(listView.isItemChecked(cursor.getPosition()));
+                final int position = cursor.getPosition();
+                checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                        listView.setItemChecked(position, isChecked);
+                    }
+                });
+            }
+        }
+    }
+
 }
