0f5a49fdd4c7da2c4e13fc8489431e4c6ac2fca0
Daniel Martí <mvdan@mvdan.cc>
2014-12-31 00:11:33 +0100

    Adapt remove-unused-trans to gradle, yet to be tested

--- F-Droid/tools/remove-unused-trans.sh
+++ F-Droid/tools/remove-unused-trans.sh
@@ -2,12 +2,11 @@
 
 # Remove extra translations
 
-lint . --quiet --check ExtraTranslation --nolines | \
-	sed -n 's@res/values-[^/]\+/\([^\.]\+\)\.xml:.*Error: "\([^"]*\)" is translated here but not found in default locale.*@\1 \2@p' | \
-	while read file name; do
-		if [[ $file == strings ]]; then
-			sed -i "/name=\"$name\"/d" res/values-*/strings.xml
-		elif [[ $file == array ]]; then
-			sed -i "/<string-array name=\"$name\"/,/<\/string-array/d" res/values-*/array.xml
-		fi
-	done
+sed -n 's@res/values-[^/]\+/\([^\.]\+\)\.xml:.*Error: "\([^"]*\)" is translated here but not found in default locale.*@\1 \2@p' < build/outputs/lint-results.txt | \
+while read file name; do
+	if [[ $file == strings ]]; then
+		sed -i "/name=\"$name\"/d" res/values-*/strings.xml
+	elif [[ $file == array ]]; then
+		sed -i "/<string-array name=\"$name\"/,/<\/string-array/d" res/values-*/array.xml
+	fi
+done
