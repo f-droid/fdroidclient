92430e163e1777bbee9447e59fab5ea8dbe0d165
Peter Serwylo <peter@serwylo.com>
2015-05-14 18:40:25 +1000

    WIP

--- F-Droid/src/org/fdroid/fdroid/net/DownloaderFactory.java
+++ F-Droid/src/org/fdroid/fdroid/net/DownloaderFactory.java
@@ -1,26 +1,37 @@
 package org.fdroid.fdroid.net;
 
 import android.content.Context;
+import android.net.Uri;
 
 import java.io.File;
 import java.io.IOException;
 
 public class DownloaderFactory {
 
-    public static Downloader create(String url, Context context)
-            throws IOException {
-        if (isOnionAddress(url)) {
+    public static Downloader create(String url, Context context) throws IOException {
+        Uri uri = Uri.parse(url);
+        if (isBluetoothAddress(uri)) {
+            return new BluetoothDownloader(null, uri.getPath(), context);
+        } else if (isOnionAddress(url)) {
             return new TorHttpDownloader(url, context);
+        } else {
+            return new HttpDownloader(url, context);
         }
-        return new HttpDownloader(url, context);
     }
 
-    public static Downloader create(String url, File destFile)
-            throws IOException {
-        if (isOnionAddress(url)) {
+    public static Downloader create(String url, File destFile) throws IOException {
+        Uri uri = Uri.parse(url);
+        if (isBluetoothAddress(uri)) {
+            return new BluetoothDownloader(null, uri.getPath(), destFile);
+        } else if (isOnionAddress(url)) {
             return new TorHttpDownloader(url, destFile);
+        } else {
+            return new HttpDownloader(url, destFile);
         }
-        return new HttpDownloader(url, destFile);
+    }
+
+    private static boolean isBluetoothAddress(Uri uri) {
+        return "bluetooth".equalsIgnoreCase(uri.getScheme());
     }
 
     private static boolean isOnionAddress(String url) {
