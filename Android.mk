886919adb745d906b136d42b31defa5598056c03
Simon Wörne <mail@simon-woerner.de>
2015-05-15 00:25:44 +0200

    fixed apk output path

--- Android.mk
+++ Android.mk
@@ -6,14 +6,15 @@ LOCAL_MODULE := F-Droid
 LOCAL_MODULE_TAGS := optional
 LOCAL_PACKAGE_NAME := F-Droid
 
-fdroid_dir := $(LOCAL_PATH)/F-Droid
-fdroid_apk := build/outputs/apk/F-Droid-release-unsigned.apk
+fdroid_root  := $(LOCAL_PATH)
+fdroid_dir   := F-Droid
+fdroid_apk   := build/outputs/apk/F-Droid-release-unsigned.apk
 
-$(LOCAL_PATH)/$(fdroid_apk):
-	cd $(fdroid_dir) && gradle assembleRelease
+$(fdroid_root)/$(fdroid_dir)/$(fdroid_apk):
+	cd $(fdroid_root)/$(fdroid_dir) && gradle assembleRelease
 
 LOCAL_CERTIFICATE := platform
-LOCAL_SRC_FILES := $(fdroid_apk)
+LOCAL_SRC_FILES := $(fdroid_dir)/$(fdroid_apk)
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 
