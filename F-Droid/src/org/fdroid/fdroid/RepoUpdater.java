012cdce37b02dbc319d3a75f11a152159362b977
Peter Serwylo <peter@serwylo.com>
2015-09-14 07:43:19 +1000

    Make closing of `Downloader`s more concise.
    The base `Downloader` class now wraps the `InputStream` returned by
    any child classes, in order to notify the child class when that stream
    is closed. This prevents each child class having to figure out a way
    to be notified of this.
    
    Also removed some dead code which was unused.

--- F-Droid/src/org/fdroid/fdroid/RepoUpdater.java
+++ F-Droid/src/org/fdroid/fdroid/RepoUpdater.java
@@ -108,7 +108,6 @@ public class RepoUpdater {
         } catch (IOException e) {
             if (downloader != null && downloader.getFile() != null) {
                 downloader.getFile().delete();
-                downloader.close();
             }
 
             throw new UpdateException(repo, "Error getting index file from " + repo.address, e);
@@ -133,8 +132,6 @@ public class RepoUpdater {
             // successful download, then we will have a file ready to use:
             processDownloadedFile(downloader.getFile(), downloader.getCacheTag());
         }
-
-        downloader.close();
     }
 
     protected void processDownloadedFile(File downloadedFile, String cacheTag) throws UpdateException {
