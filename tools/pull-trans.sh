7440cf1b742cc16d22ec26bbe7b35af2462ff17e
Daniel Martí <mvdan@mvdan.cc>
2015-09-17 19:18:18 -0700

    Add script to pull+squash from weblate. Fixes #140

---
+++ tools/pull-trans.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+# This script pulls translations from weblate.
+#
+# It squashes all the changes into a single commit. This removes authorship
+# from the changes, which is given to the Translatebot, so to keep the names
+# they are grabbed from git log and added to the commit message.
+#
+# Note that this will apply changes and commit them! Make sure to not have
+# uncommited changes when running this script.
+
+REMOTE="weblate"
+REMOTE_URL="git://git.weblate.org/f-droid.git"
+REMOTE_BRANCH="master"
+
+AUTHOR="F-Droid Translatebot <admin@f-droid.org>"
+
+if ! git ls-remote --exit-code $REMOTE >/dev/null 2>/dev/null; then
+	echo "Remote doesn't exist! Try the following:"
+	echo "  git remote add $REMOTE $REMOTE_URL"
+	echo "  git fetch $REMOTE"
+	exit 1
+fi
+
+files=$(find . -wholename '*/values-*/strings.xml')
+
+ref="${REMOTE}/${REMOTE_BRANCH}"
+diff="HEAD..$ref -- $files"
+
+authors=$(git log --format="%s %an" $diff | \
+	sed 's/Translated using Weblate (\(.*\)) \(.*\)/\2||\1/' | sort -f -u | column -s '||' -t)
+
+git diff $diff | git apply
+
+git add $files
+
+git commit --author "$AUTHOR" -m "Pull translation updates from weblate
+
+Translators:
+
+$authors"
