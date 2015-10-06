d1bdd7789b15d337887c021d73f76d265243d194
Daniel Martí <mvdan@mvdan.cc>
2015-10-06 14:06:22 +0200

    Remove now unused vendored jmdns

--- extern/jmdns/src/main/java/javax/jmdns/package-info.java
+++
@@ -1,9 +0,0 @@
-package javax.jmdns;
-
-/**
- * JmDNS is a Java implementation of multi-cast DNS and can be used for service registration and discovery in local area networks. JmDNS is fully compatible with Apple's Bonjour. The project was originally started in December 2002 by Arthur van Hoff
- * at Strangeberry. In November 2003 the project was moved to SourceForge, and the name was changed from JRendezvous to JmDNS for legal reasons. Many thanks to Stuart Cheshire for help and moral support.
- * <p>
- * <a href="http://jmdns.sourceforge.net/">http://jmdns.sourceforge.net/<a>
- * </p>
- **/
