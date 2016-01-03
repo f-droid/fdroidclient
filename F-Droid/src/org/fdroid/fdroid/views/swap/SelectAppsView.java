f-droid/fdroidclient

commit f2212e33d7fea270fe9167fbff5c349285a4361b

Make newer checkstyle happy
Mostly just dropping public from constructors.

F-Droid/src/org/fdroid/fdroid/views/swap/SelectAppsView.java
@@ -43,20 +43,20 @@ public class SelectAppsView extends ListView implements
         LoaderManager.LoaderCallbacks<Cursor>,
         SearchView.OnQueryTextListener {
 
-    public SelectAppsView(Context context) {
+    SelectAppsView(Context context) {
         super(context);
     }
 
-    public SelectAppsView(Context context, AttributeSet attrs) {
+    SelectAppsView(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
-    public SelectAppsView(Context context, AttributeSet attrs, int defStyleAttr) {
+    SelectAppsView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    public SelectAppsView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+    SelectAppsView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
     }
 
@@ -223,7 +223,7 @@ public class SelectAppsView extends ListView implements
         @NonNull
         private final ListView listView;
 
-        public AppListAdapter(@NonNull ListView listView, @NonNull Context context, @Nullable Cursor c) {
+        AppListAdapter(@NonNull ListView listView, @NonNull Context context, @Nullable Cursor c) {
             super(context, c, FLAG_REGISTER_CONTENT_OBSERVER);
             this.listView = listView;
         }

F-Droid/src/org/fdroid/fdroid/AppDetails.java
@@ -147,17 +147,17 @@ public class AppDetails extends AppCompatActivity implements ProgressListener, A
 
     class AppObserver extends ContentObserver {
 
-        public AppObserver(Handler handler) {
+        AppObserver(Handler handler) {
             super(handler);
         }
 
         @Override
-        public void onChange(boolean selfChange) {
+        void onChange(boolean selfChange) {
             onChange(selfChange, null);
         }
 
         @Override
-        public void onChange(boolean selfChange, Uri uri) {
+        void onChange(boolean selfChange, Uri uri) {
             onAppChanged();
         }
 
@@ -168,7 +168,7 @@ public class AppDetails extends AppCompatActivity implements ProgressListener, A
         private final LayoutInflater mInflater = (LayoutInflater) mctx.getSystemService(
                 Context.LAYOUT_INFLATER_SERVICE);
 
-        public ApkListAdapter(Context context, App app) {
+        ApkListAdapter(Context context, App app) {
             super(context, 0);
             final List<Apk> apks = ApkProvider.Helper.findByApp(context, app.id);
             for (final Apk apk : apks) {
@@ -344,7 +344,7 @@ public class AppDetails extends AppCompatActivity implements ProgressListener, A
         public final ApkDownloader downloader;
         public final App app;
 
-        public ConfigurationChangeHelper(ApkDownloader downloader, App app) {
+        ConfigurationChangeHelper(ApkDownloader downloader, App app) {
             this.downloader = downloader;
             this.app = app;
         }

F-Droid/src/org/fdroid/fdroid/FDroid.java
@@ -365,7 +365,7 @@ public class FDroid extends ActionBarActivity {
 
     private class AppObserver extends ContentObserver {
 
-        public AppObserver() {
+        AppObserver() {
             super(null);
         }
 

F-Droid/src/org/fdroid/fdroid/compat/PRNGFixes.java
@@ -146,7 +146,7 @@ public final class PRNGFixes extends Compatibility {
     @SuppressWarnings("serial")
     private static class LinuxPRNGSecureRandomProvider extends Provider {
 
-        public LinuxPRNGSecureRandomProvider() {
+        LinuxPRNGSecureRandomProvider() {
             super("LinuxPRNG",
                     1.0,
                     "A Linux-specific random number provider that uses"

F-Droid/src/org/fdroid/fdroid/data/AppProvider.java
@@ -241,21 +241,21 @@ public class AppProvider extends FDroidProvider {
 
         private boolean naturalJoinToInstalled;
 
-        public AppQuerySelection() {
+        AppQuerySelection() {
             // The same as no selection, because "1" will always resolve to true when executing the SQL query.
             // e.g. "WHERE 1 AND ..." is the same as "WHERE ..."
             super("1");
         }
 
-        public AppQuerySelection(String selection) {
+        AppQuerySelection(String selection) {
             super(selection);
         }
 
-        public AppQuerySelection(String selection, String[] args) {
+        AppQuerySelection(String selection, String[] args) {
             super(selection, args);
         }
 
-        public AppQuerySelection(String selection, List<String> args) {
+        AppQuerySelection(String selection, List<String> args) {
             super(selection, args);
         }
 

F-Droid/src/org/fdroid/fdroid/data/Repo.java
@@ -124,6 +124,7 @@ public class Repo extends ValueObject {
     public boolean hasBeenUpdated() {
         return this.lastetag != null;
     }
+
     /**
      * If we haven't run an update for this repo yet, then the name
      * will be unknown, in which case we will just take a guess at an

F-Droid/src/org/fdroid/fdroid/localrepo/type/BluetoothSwap.java
@@ -164,7 +164,7 @@ public final class BluetoothSwap extends SwapType {
 
     private static class NoBluetoothType extends SwapType {
 
-        public NoBluetoothType(@NonNull Context context) {
+        NoBluetoothType(@NonNull Context context) {
             super(context);
         }
 

F-Droid/src/org/fdroid/fdroid/net/AsyncDownloadWrapper.java
@@ -28,7 +28,7 @@ class AsyncDownloadWrapper extends Handler implements AsyncDownloader {
      * world about completion. Therefore, we require the listener as a
      * parameter to the constructor.
      */
-    public AsyncDownloadWrapper(Downloader downloader, Listener listener) {
+    AsyncDownloadWrapper(Downloader downloader, Listener listener) {
         this.downloader = downloader;
         this.listener = listener;
     }

commit d24a6b71b9ba43f770338851d5d4c9361412be22

Partially revert f2212e33. Make some constructors public again.
There is some code in the Android SDK using reflection to find the constructor
of one of our custom views. As such, a previous change in f2212e33 broke this
code because it made constructors package local. This partially reverts the bit
of f2212e33 which pertains to the constructors accessed using reflection.

F-Droid/src/org/fdroid/fdroid/views/swap/SelectAppsView.java
@@ -43,20 +43,20 @@ public class SelectAppsView extends ListView implements
         LoaderManager.LoaderCallbacks<Cursor>,
         SearchView.OnQueryTextListener {
 
-    SelectAppsView(Context context) {
+    public SelectAppsView(Context context) {
         super(context);
     }
 
-    SelectAppsView(Context context, AttributeSet attrs) {
+    public SelectAppsView(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
-    SelectAppsView(Context context, AttributeSet attrs, int defStyleAttr) {
+    public SelectAppsView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    SelectAppsView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+    public SelectAppsView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
     }
 