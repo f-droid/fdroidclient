f-droid/fdroidclient

commit 8e193cecffd9a5cdd46051bb83f63d5032a6a7b7

Apk: Don't create Integer object just to compare

F-Droid/src/org/fdroid/fdroid/data/Apk.java
@@ -146,7 +146,7 @@ public class Apk extends ValueObject implements Comparable<Apk> {
 
     @Override
     public int compareTo(Apk apk) {
-        return Integer.valueOf(vercode).compareTo(apk.vercode);
+        return Integer.compare(vercode, apk.vercode);
     }
 
 }

commit 5179e032dbc26f99f220bb5ab5bac7f24e6398a5

Fix NewApi bug introduced in 8e193cec

F-Droid/src/org/fdroid/fdroid/data/Apk.java
@@ -1,7 +1,9 @@
 package org.fdroid.fdroid.data;
 
+import android.annotation.TargetApi;
 import android.content.ContentValues;
 import android.database.Cursor;
+import android.os.Build;
 
 import org.fdroid.fdroid.Utils;
 
@@ -145,7 +147,11 @@ public class Apk extends ValueObject implements Comparable<Apk> {
     }
 
     @Override
+    @TargetApi(19)
     public int compareTo(Apk apk) {
+        if (Build.VERSION.SDK_INT < 19) {
+            return Integer.valueOf(vercode).compareTo(apk.vercode);
+        }
         return Integer.compare(vercode, apk.vercode);
     }
 