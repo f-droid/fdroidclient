96fe1f9584daa4b674c8bfaae9d7b7285edcb482
Hans-Christoph Steiner <hans@eds.org>
2016-02-26 20:35:31 +0100

    do not show "Swap" as an option on less than android-10
    Swap requires lots of APIs that are only all available in android-10, so
    hide the menu item on older platforms.
    
    closes #581 https://gitlab.com/fdroid/fdroidclient/issues/581
    closes #575 https://gitlab.com/fdroid/fdroidclient/issues/575

--- F-Droid/src/org/fdroid/fdroid/FDroid.java
+++ F-Droid/src/org/fdroid/fdroid/FDroid.java
@@ -28,6 +28,7 @@ import android.content.Intent;
 import android.content.res.Configuration;
 import android.database.ContentObserver;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -289,6 +290,10 @@ public class FDroid extends AppCompatActivity implements SearchView.OnQueryTextL
             // ignore on devices without Bluetooth
             MenuItem btItem = menu.findItem(R.id.action_bluetooth_apk);
             btItem.setVisible(false);
+        }
+        if (Build.VERSION.SDK_INT < 10) {
+            MenuItem menuItem = menu.findItem(R.id.action_swap);
+            menuItem.setVisible(false);
         }
 
         SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
