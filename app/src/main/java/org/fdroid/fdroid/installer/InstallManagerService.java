f-droid/fdroidclient

commit f9a30d2e1c9d95302e8f4dd0e733d019f70bed66

reuse Notification.Builder instances
This is the recommended way to deal with updating Notifications. Each new
update should be a new Notification, but the same Builder instance should
be used to generate each new Notification instance.

app/src/main/java/org/fdroid/fdroid/installer/InstallManagerService.java
@@ -165,10 +165,10 @@ public class InstallManagerService extends Service {
         Apk apk = new Apk(intent.getParcelableExtra(EXTRA_APK));
         addToActive(urlString, app, apk);
 
-        Notification notification = createNotification(intent.getDataString(), apk).build();
-        notificationManager.notify(urlString.hashCode(), notification);
+        NotificationCompat.Builder builder = createNotificationBuilder(intent.getDataString(), apk);
+        notificationManager.notify(urlString.hashCode(), builder.build());
 
-        registerDownloaderReceivers(urlString);
+        registerDownloaderReceivers(urlString, builder);
 
         File apkFilePath = Utils.getApkDownloadPath(this, intent.getData());
         long apkFileSize = apkFilePath.length();
@@ -215,7 +215,7 @@ public class InstallManagerService extends Service {
         }
     }
 
-    private void registerDownloaderReceivers(String urlString) {
+    private void registerDownloaderReceivers(String urlString, final NotificationCompat.Builder builder) {
         BroadcastReceiver startedReceiver = new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
@@ -225,13 +225,10 @@ public class InstallManagerService extends Service {
             @Override
             public void onReceive(Context context, Intent intent) {
                 String urlString = intent.getDataString();
-                Apk apk = ACTIVE_APKS.get(urlString);
                 int bytesRead = intent.getIntExtra(Downloader.EXTRA_BYTES_READ, 0);
                 int totalBytes = intent.getIntExtra(Downloader.EXTRA_TOTAL_BYTES, 0);
-                Notification notification = createNotification(urlString, apk)
-                        .setProgress(totalBytes, bytesRead, false)
-                        .build();
-                notificationManager.notify(urlString.hashCode(), notification);
+                builder.setProgress(totalBytes, bytesRead, false);
+                notificationManager.notify(urlString.hashCode(), builder.build());
             }
         };
         BroadcastReceiver completeReceiver = new BroadcastReceiver() {
@@ -243,7 +240,7 @@ public class InstallManagerService extends Service {
                 if (AppDetails.isAppVisible(apk.packageName)) {
                     cancelNotification(urlString);
                 } else {
-                    notifyDownloadComplete(apk, urlString);
+                    notifyDownloadComplete(urlString, builder, apk);
                 }
                 unregisterDownloaderReceivers(urlString);
             }
@@ -270,7 +267,7 @@ public class InstallManagerService extends Service {
         });
     }
 
-    private NotificationCompat.Builder createNotification(String urlString, Apk apk) {
+    private NotificationCompat.Builder createNotificationBuilder(String urlString, Apk apk) {
         int downloadUrlId = urlString.hashCode();
         return new NotificationCompat.Builder(this)
                 .setAutoCancel(false)
@@ -317,7 +314,7 @@ public class InstallManagerService extends Service {
      * Post a notification about a completed download.  {@code packageName} must be a valid
      * and currently in the app index database.
      */
-    private void notifyDownloadComplete(Apk apk, String urlString) {
+    private void notifyDownloadComplete(String urlString, NotificationCompat.Builder builder, Apk apk) {
         String title;
         try {
             PackageManager pm = getPackageManager();
@@ -328,15 +325,12 @@ public class InstallManagerService extends Service {
         }
 
         int downloadUrlId = urlString.hashCode();
-        NotificationCompat.Builder builder =
-                new NotificationCompat.Builder(this)
-                        .setAutoCancel(true)
-                        .setContentTitle(title)
-                        .setSmallIcon(android.R.drawable.stat_sys_download_done)
-                        .setContentIntent(getAppDetailsIntent(downloadUrlId, apk))
-                        .setContentText(getString(R.string.tap_to_install));
-        NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        nm.notify(downloadUrlId, builder.build());
+        builder.setAutoCancel(true)
+                .setOngoing(false)
+                .setContentTitle(title)
+                .setSmallIcon(android.R.drawable.stat_sys_download_done)
+                .setContentText(getString(R.string.tap_to_install));
+        notificationManager.notify(downloadUrlId, builder.build());
     }
 
     /**

commit 5f623e0c4a0bf0bd2ce4319aa6d43217333b98a7

after downloading is complete, update notification to "Tap to install"
f9a30d2e1c9d95302e8f4dd0e733d019f70bed66 broke the "Tap to install" aspect.

app/src/main/java/org/fdroid/fdroid/installer/InstallManagerService.java
@@ -240,7 +240,7 @@ public class InstallManagerService extends Service {
                 if (AppDetails.isAppVisible(apk.packageName)) {
                     cancelNotification(urlString);
                 } else {
-                    notifyDownloadComplete(urlString, builder, apk);
+                    notifyDownloadComplete(urlString, apk);
                 }
                 unregisterDownloaderReceivers(urlString);
             }
@@ -312,9 +312,14 @@ public class InstallManagerService extends Service {
 
     /**
      * Post a notification about a completed download.  {@code packageName} must be a valid
-     * and currently in the app index database.
+     * and currently in the app index database.  This must create a new {@code Builder}
+     * instance otherwise the progress/cancel stuff does not go away.
+     *
+     * @see <a href=https://code.google.com/p/android/issues/detail?id=47809> Issue 47809:
+     * Removing the progress bar from a notification should cause the notification's content
+     * text to return to normal size</a>
      */
-    private void notifyDownloadComplete(String urlString, NotificationCompat.Builder builder, Apk apk) {
+    private void notifyDownloadComplete(String urlString, Apk apk) {
         String title;
         try {
             PackageManager pm = getPackageManager();
@@ -325,12 +330,16 @@ public class InstallManagerService extends Service {
         }
 
         int downloadUrlId = urlString.hashCode();
-        builder.setAutoCancel(true)
+        notificationManager.cancel(downloadUrlId);
+        Notification notification = new NotificationCompat.Builder(this)
+                .setAutoCancel(true)
                 .setOngoing(false)
                 .setContentTitle(title)
+                .setContentIntent(getAppDetailsIntent(downloadUrlId, apk))
                 .setSmallIcon(android.R.drawable.stat_sys_download_done)
-                .setContentText(getString(R.string.tap_to_install));
-        notificationManager.notify(downloadUrlId, builder.build());
+                .setContentText(getString(R.string.tap_to_install))
+                .build();
+        notificationManager.notify(downloadUrlId, notification);
     }
 
     /**