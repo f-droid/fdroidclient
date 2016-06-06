60451a050f757777748b24367728ac966b28b799
Peter Serwylo <peter@serwylo.com>
2016-06-06 23:56:46 +1000

    Remove unused test code.
    Many of the `Mock*` classes are there to deal with idiosyncrosies of
    the Android SDK, including `final`/package local/`@Hide` annotations/etc.
    They are no longer required with robolectric tests.

--- app/src/androidTest/java/org/fdroid/fdroid/TestUtils.java
+++ app/src/androidTest/java/org/fdroid/fdroid/TestUtils.java
@@ -1,143 +1,20 @@
 package org.fdroid.fdroid;
 
 import android.app.Instrumentation;
-import android.content.ContentResolver;
-import android.content.ContentValues;
 import android.content.Context;
-import android.net.Uri;
 import android.os.Environment;
 import android.support.annotation.Nullable;
 import android.util.Log;
-
-import junit.framework.AssertionFailedError;
-
-import org.fdroid.fdroid.data.ApkProvider;
-import org.fdroid.fdroid.data.AppProvider;
-import org.fdroid.fdroid.data.FDroidProviderTestOld;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 public class TestUtils {
 
     private static final String TAG = "TestUtils";
-
-    public static <T extends Comparable> void assertContainsOnly(List<T> actualList, T[] expectedArray) {
-        List<T> expectedList = new ArrayList<>(expectedArray.length);
-        Collections.addAll(expectedList, expectedArray);
-        assertContainsOnly(actualList, expectedList);
-    }
-
-    public static <T extends Comparable> void assertContainsOnly(T[] actualArray, List<T> expectedList) {
-        List<T> actualList = new ArrayList<>(actualArray.length);
-        Collections.addAll(actualList, actualArray);
-        assertContainsOnly(actualList, expectedList);
-    }
-
-    public static <T extends Comparable> void assertContainsOnly(T[] actualArray, T[] expectedArray) {
-        List<T> expectedList = new ArrayList<>(expectedArray.length);
-        Collections.addAll(expectedList, expectedArray);
-        assertContainsOnly(actualArray, expectedList);
-    }
-
-    public static <T> String listToString(List<T> list) {
-        String string = "[";
-        for (int i = 0; i < list.size(); i++) {
-            if (i > 0) {
-                string += ", ";
-            }
-            string += "'" + list.get(i) + "'";
-        }
-        string += "]";
-        return string;
-    }
-
-    public static <T extends Comparable> void assertContainsOnly(List<T> actualList, List<T> expectedContains) {
-        if (actualList.size() != expectedContains.size()) {
-            String message =
-                    "List sizes don't match.\n" +
-                    "Expected: " +
-                    listToString(expectedContains) + "\n" +
-                    "Actual:   " +
-                    listToString(actualList);
-            throw new AssertionFailedError(message);
-        }
-        for (T required : expectedContains) {
-            boolean containsRequired = false;
-            for (T itemInList : actualList) {
-                if (required.equals(itemInList)) {
-                    containsRequired = true;
-                    break;
-                }
-            }
-            if (!containsRequired) {
-                String message =
-                        "List doesn't contain \"" + required + "\".\n" +
-                        "Expected: " +
-                        listToString(expectedContains) + "\n" +
-                        "Actual:   " +
-                        listToString(actualList);
-                throw new AssertionFailedError(message);
-            }
-        }
-    }
-
-    public static void insertApp(ContentResolver resolver, String appId, String name) {
-        insertApp(resolver, appId, name, new ContentValues());
-    }
-
-    public static void insertApp(ContentResolver resolver, String id, String name, ContentValues additionalValues) {
-
-        ContentValues values = new ContentValues();
-        values.put(AppProvider.DataColumns.PACKAGE_NAME, id);
-        values.put(AppProvider.DataColumns.NAME, name);
-
-        // Required fields (NOT NULL in the database).
-        values.put(AppProvider.DataColumns.SUMMARY, "test summary");
-        values.put(AppProvider.DataColumns.DESCRIPTION, "test description");
-        values.put(AppProvider.DataColumns.LICENSE, "GPL?");
-        values.put(AppProvider.DataColumns.IS_COMPATIBLE, 1);
-        values.put(AppProvider.DataColumns.IGNORE_ALLUPDATES, 0);
-        values.put(AppProvider.DataColumns.IGNORE_THISUPDATE, 0);
-
-        values.putAll(additionalValues);
-
-        Uri uri = AppProvider.getContentUri();
-
-        resolver.insert(uri, values);
-    }
-
-    public static Uri insertApk(FDroidProviderTestOld<ApkProvider> providerTest, String id, int versionCode) {
-        return insertApk(providerTest, id, versionCode, new ContentValues());
-    }
-
-    public static Uri insertApk(FDroidProviderTestOld<ApkProvider> providerTest, String id, int versionCode, ContentValues additionalValues) {
-
-        ContentValues values = new ContentValues();
-
-        values.put(ApkProvider.DataColumns.PACKAGE_NAME, id);
-        values.put(ApkProvider.DataColumns.VERSION_CODE, versionCode);
-
-        // Required fields (NOT NULL in the database).
-        values.put(ApkProvider.DataColumns.REPO_ID, 1);
-        values.put(ApkProvider.DataColumns.VERSION_NAME, "The good one");
-        values.put(ApkProvider.DataColumns.HASH, "11111111aaaaaaaa");
-        values.put(ApkProvider.DataColumns.NAME, "Test Apk");
-        values.put(ApkProvider.DataColumns.SIZE, 10000);
-        values.put(ApkProvider.DataColumns.IS_COMPATIBLE, 1);
-
-        values.putAll(additionalValues);
-
-        Uri uri = ApkProvider.getContentUri();
-
-        return providerTest.getMockContentResolver().insert(uri, values);
-    }
 
     @Nullable
     public static File copyAssetToDir(Context context, String assetName, File directory) {
