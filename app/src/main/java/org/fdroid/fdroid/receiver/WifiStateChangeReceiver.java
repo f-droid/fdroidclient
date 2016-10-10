a5a90954bc8bc7fdf83969c9121da87aab02d8c6
Hans-Christoph Steiner <hans@eds.org>
2016-10-10 20:13:41 +0200

    fix lint UnsafeProtectedBroadcastReceiver
    Android won't protect us from other apps sending other Intents to these
    receivers, so at least check that the action string matches what its
    looking for.  This is based on a lint recommendation.

--- app/src/main/java/org/fdroid/fdroid/receiver/WifiStateChangeReceiver.java
+++ app/src/main/java/org/fdroid/fdroid/receiver/WifiStateChangeReceiver.java
@@ -4,14 +4,21 @@ import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.net.wifi.WifiManager;
 
+import org.fdroid.fdroid.Utils;
 import org.fdroid.fdroid.net.WifiStateChangeService;
 
 public class WifiStateChangeReceiver extends BroadcastReceiver {
+    private static final String TAG = "WifiStateChangeReceiver";
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        intent.setComponent(new ComponentName(context, WifiStateChangeService.class));
-        context.startService(intent);
+        if (WifiManager.NETWORK_STATE_CHANGED_ACTION.equals(intent.getAction())) {
+            intent.setComponent(new ComponentName(context, WifiStateChangeService.class));
+            context.startService(intent);
+        } else {
+            Utils.debugLog(TAG, "received unsupported Intent: " + intent);
+        }
     }
 }
