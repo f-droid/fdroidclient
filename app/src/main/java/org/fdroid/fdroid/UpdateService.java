7e7ec966ee33958c26d80324047f313f5d5e6cdc
Hans-Christoph Steiner <hans@eds.org>
2016-11-10 16:00:11 +0100

    improved internet state handling for updates, including metered
    This introduces three network states:
    
    1. completely disconnected
    2. connected only via metered networks
    3. connected via unlimited networks
    
    This allows the update process to use bandwidth better, especially when the
    user has enabled the "Only on WiFi" setting.  It also helps prevent silly,
    cryptic error messages in the update process is triggered when there isn't
    internet available.
    
    I tested this with:
    
    * 4G only, but not set up for internet
    * 4G only, with internet
    * 4G + WiFi
    * WiFi only airplane mode
    * no internet at all, full airplane mode
    
    closes #793
    closes #774

--- app/src/main/java/org/fdroid/fdroid/UpdateService.java
+++ app/src/main/java/org/fdroid/fdroid/UpdateService.java
@@ -31,6 +31,8 @@ import android.database.Cursor;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Build;
+import android.os.Handler;
+import android.os.Looper;
 import android.os.Process;
 import android.os.SystemClock;
 import android.preference.PreferenceManager;
@@ -78,6 +80,12 @@ public class UpdateService extends IntentService {
 
     private static final int NOTIFY_ID_UPDATING = 0;
     private static final int NOTIFY_ID_UPDATES_AVAILABLE = 1;
+
+    private static final int FLAG_NET_UNAVAILABLE = 0;
+    private static final int FLAG_NET_METERED = 1;
+    private static final int FLAG_NET_NO_LIMIT = 2;
+
+    private static Handler toastHandler;
 
     private NotificationManager notificationManager;
     private NotificationCompat.Builder notificationBuilder;
@@ -279,29 +287,50 @@ public class UpdateService extends IntentService {
             return false;
         }
 
-        return isNetworkAvailableForUpdate(this);
+        return true;
     }
 
     /**
-     * If we are to update the repos only on wifi, make sure that connection is active
+     * Gets the state of internet availability, whether there is no connection at all,
+     * whether the connection has no usage limit (like most WiFi), or whether this is
+     * a metered connection like most cellular plans or hotspot WiFi connections.
      */
-    private static boolean isNetworkAvailableForUpdate(Context context) {
+    private static int getNetworkState(Context context) {
         ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
 
-        // this could be cellular or wifi
         NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
-        if (activeNetwork == null) {
-            return false;
+        if (activeNetwork == null || !activeNetwork.isConnected()) {
+            return FLAG_NET_UNAVAILABLE;
         }
 
         int networkType = activeNetwork.getType();
         switch (networkType) {
             case ConnectivityManager.TYPE_ETHERNET:
             case ConnectivityManager.TYPE_WIFI:
-                return activeNetwork.isConnectedOrConnecting();
+                if (Build.VERSION.SDK_INT >= 16 && cm.isActiveNetworkMetered()) {
+                    return FLAG_NET_METERED;
+                } else {
+                    return FLAG_NET_NO_LIMIT;
+                }
             default:
-                return Preferences.get().isUpdateOnlyOnWifi();
+                return FLAG_NET_METERED;
         }
+    }
+
+    /**
+     * In order to send a {@link Toast} from a {@link IntentService}, we have to do these tricks.
+     */
+    private void sendNoInternetToast() {
+        if (toastHandler == null) {
+            toastHandler = new Handler(Looper.getMainLooper());
+        }
+        toastHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(getApplicationContext(),
+                        R.string.warning_no_internet, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
@@ -318,9 +347,21 @@ public class UpdateService extends IntentService {
 
         try {
             // See if it's time to actually do anything yet...
-            if (manualUpdate) {
-                Utils.debugLog(TAG, "Unscheduled (manually requested) update");
-            } else if (!verifyIsTimeForScheduledRun()) {
+            int netState = getNetworkState(this);
+            if (netState == FLAG_NET_UNAVAILABLE) {
+                Utils.debugLog(TAG, "No internet, cannot update");
+                if (manualUpdate) {
+                    sendNoInternetToast();
+                }
+                return;
+            }
+
+            if (manualUpdate || (netState == FLAG_NET_NO_LIMIT)) {
+                // triggered by the user, or by connecting to WiFi, etc.
+                Utils.debugLog(TAG, "manually requested update or on unlimited internet");
+            } else if (Preferences.get().isUpdateOnlyOnUnmeteredNetworks()
+                    || !verifyIsTimeForScheduledRun()) {
+                Utils.debugLog(TAG, "don't run update, we're on metered internet");
                 return;
             }
 
