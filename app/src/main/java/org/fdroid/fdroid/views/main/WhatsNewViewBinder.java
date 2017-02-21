34e176539b350916f4b99235a20c697c493ee58b
Peter Serwylo <peter@serwylo.com>
2017-02-21 11:37:06 +1100

    Add swipe-down-to-refresh-repos on main overview screen.
    Happy to discuss whether this is a good idea or not, but right now
    there is no way to update repositories so often you are left with
    an empty first screen.
    
    This doesn't worry about state management (e.g. remembering
    whether we are refreshing or not and showing this when we resume the
    activity). Instead, it listens for the refresh listener, and when
    triggered it will set the refreshing state to not refreshing. For now
    the notification can act as the feedback that something is happening.

--- app/src/main/java/org/fdroid/fdroid/views/main/WhatsNewViewBinder.java
+++ app/src/main/java/org/fdroid/fdroid/views/main/WhatsNewViewBinder.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -12,6 +13,7 @@ import android.view.View;
 import android.widget.FrameLayout;
 
 import org.fdroid.fdroid.R;
+import org.fdroid.fdroid.UpdateService;
 import org.fdroid.fdroid.data.AppProvider;
 import org.fdroid.fdroid.data.Schema;
 import org.fdroid.fdroid.views.whatsnew.WhatsNewAdapter;
@@ -28,7 +30,7 @@ class WhatsNewViewBinder implements LoaderManager.LoaderCallbacks<Cursor> {
 
     private static RecyclerView.ItemDecoration appListDecorator;
 
-    WhatsNewViewBinder(AppCompatActivity activity, FrameLayout parent) {
+    WhatsNewViewBinder(final AppCompatActivity activity, FrameLayout parent) {
         this.activity = activity;
 
         View whatsNewView = activity.getLayoutInflater().inflate(R.layout.main_tab_whats_new, parent, true);
@@ -56,6 +58,15 @@ class WhatsNewViewBinder implements LoaderManager.LoaderCallbacks<Cursor> {
         }
 
         appList.addItemDecoration(appListDecorator);
+
+        final SwipeRefreshLayout swipeToRefresh = (SwipeRefreshLayout) whatsNewView.findViewById(R.id.swipe_to_refresh);
+        swipeToRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                swipeToRefresh.setRefreshing(false);
+                UpdateService.updateNow(activity);
+            }
+        });
 
         activity.getSupportLoaderManager().initLoader(LOADER_ID, null, this);
     }
