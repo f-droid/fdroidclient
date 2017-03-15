0581029bf10f16f6077afe8dca729bc5ae1a7b4e
Peter Serwylo <peter@serwylo.com>
2017-03-15 12:25:38 +1100

    Respond to wifi being enabled by hiding 'no internet' banner

--- app/src/main/java/org/fdroid/fdroid/views/BannerNoInternet.java
+++ app/src/main/java/org/fdroid/fdroid/views/BannerNoInternet.java
@@ -1,6 +1,10 @@
 package org.fdroid.fdroid.views;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
 import android.util.AttributeSet;
 import android.view.Gravity;
 import android.view.View;
@@ -11,6 +15,12 @@ import org.fdroid.fdroid.Utils;
 /**
  * Widget which monitors the network state and shows a "No Internet" message when it identifies the
  * device is not connected. Will only monitor the wifi state when attached to the window.
+ * Note that this does a pretty poor job of responding to network changes in real time. It only
+ * knows how to respond to the _enabling_ of wifi (not disabling of wifi, nor enabling/disabling
+ * of mobile data). However it will always query the network state when it is shown to the user. This
+ * way if they change between tabs, hide and then open F-Droid, or do other things which require the
+ * view to attach to the window again then it will update the network state. In practice this works
+ * pretty well.
  */
 public class BannerNoInternet extends android.support.v7.widget.AppCompatTextView {
 
@@ -49,16 +59,29 @@ public class BannerNoInternet extends android.support.v7.widget.AppCompatTextVie
             // Don't try and query the network state if in the Android Studio UI Builder (it wont work).
             setVisibility(View.VISIBLE);
         } else {
-            Utils.NetworkState state = Utils.getNetworkState(getContext());
-            if (state == Utils.NetworkState.NET_UNAVAILABLE) {
-                setVisibility(View.VISIBLE);
-            } else {
-                setVisibility(View.GONE);
-            }
+            getContext().registerReceiver(
+                    onNetworkStateChanged, new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
+            updateNetworkState();
+        }
+    }
+
+    private void updateNetworkState() {
+        Utils.NetworkState state = Utils.getNetworkState(getContext());
+        if (state == Utils.NetworkState.NET_UNAVAILABLE) {
+            setVisibility(View.VISIBLE);
+        } else {
+            setVisibility(View.GONE);
         }
     }
 
     private void stopMonitoringNetworkState() {
-
+        getContext().unregisterReceiver(onNetworkStateChanged);
     }
+
+    private final BroadcastReceiver onNetworkStateChanged = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            updateNetworkState();
+        }
+    };
 }
