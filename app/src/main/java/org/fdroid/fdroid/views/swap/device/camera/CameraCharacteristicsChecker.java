b419b78c9452b8a9c1f939d79f74ec6892a2c693
jif <jif@gmail.com>
2018-03-01 18:35:45 +0100

    Add camera characteristics checker
    Those classes consist of 1 abstract class, which provide a factory for
    2 classes each of which implements different behaviors according to
    the Android API version.

---
+++ app/src/main/java/org/fdroid/fdroid/views/swap/device/camera/CameraCharacteristicsChecker.java
@@ -0,0 +1,20 @@
+package org.fdroid.fdroid.views.swap.device.camera;
+
+import android.content.Context;
+
+public abstract class CameraCharacteristicsChecker {
+    public static CameraCharacteristicsChecker getInstance(final Context context) {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            return new CameraCharacteristicsMinApiLevel21(context);
+        } else
+            return new CameraCharacteristicsMaxApiLevel20();
+    }
+
+    public abstract boolean hasAutofocus();
+
+    class FDroidDeviceException extends Exception {
+        FDroidDeviceException(final String message, final Throwable cause) {
+            super(message, cause);
+        }
+    }
+}
