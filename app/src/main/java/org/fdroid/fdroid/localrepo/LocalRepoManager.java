af509e431e50bfae6f4911531fbc49d89a5cdadd
Hans-Christoph Steiner <hans@eds.org>
2018-04-11 11:24:11 +0200

    reformat localrepo code in using Android Studio Ctrl-Alt-L

--- app/src/main/java/org/fdroid/fdroid/localrepo/LocalRepoManager.java
+++ app/src/main/java/org/fdroid/fdroid/localrepo/LocalRepoManager.java
@@ -14,7 +14,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
 import android.util.Log;
-
 import org.fdroid.fdroid.FDroidApp;
 import org.fdroid.fdroid.Hasher;
 import org.fdroid.fdroid.Preferences;
