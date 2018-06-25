7043627a3a7af5eabac3ab11859b6196b814a8fb
Hans-Christoph Steiner <hans@eds.org>
2018-06-25 23:18:01 +0200

    basic Install History viewer, available only when logging is enabled
    This provides a super simple way to view the install history directly in
    F-Droid.

--- app/src/main/java/org/fdroid/fdroid/installer/InstallHistoryService.java
+++ app/src/main/java/org/fdroid/fdroid/installer/InstallHistoryService.java
@@ -28,7 +28,7 @@ import android.net.Uri;
 import android.os.Process;
 import android.support.v4.content.LocalBroadcastManager;
 import android.text.TextUtils;
-
+import org.fdroid.fdroid.BuildConfig;
 import org.fdroid.fdroid.Utils;
 import org.fdroid.fdroid.data.Apk;
 
@@ -47,7 +47,8 @@ import java.util.List;
 public class InstallHistoryService extends IntentService {
     public static final String TAG = "InstallHistoryService";
 
-    public static final Uri LOG_URI = Uri.parse("content://org.fdroid.fdroid.installer/install_history/all");
+    public static final String AUTHORITY = BuildConfig.APPLICATION_ID + ".installer";
+    public static final Uri LOG_URI = Uri.parse("content://" + AUTHORITY + "/install_history/all");
 
     private static BroadcastReceiver broadcastReceiver;
 
