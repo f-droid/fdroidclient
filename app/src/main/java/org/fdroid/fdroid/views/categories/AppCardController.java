51431d59eebfb06d8fddb2b1d98f4b824d87da0e
Hans-Christoph Steiner <hans@eds.org>
2019-03-26 21:29:13 +0100

    cleanup imports

--- app/src/main/java/org/fdroid/fdroid/views/categories/AppCardController.java
+++ app/src/main/java/org/fdroid/fdroid/views/categories/AppCardController.java
@@ -2,30 +2,22 @@ package org.fdroid.fdroid.views.categories;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityOptionsCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v4.util.Pair;
-import android.support.v7.graphics.Palette;
 import android.support.v7.widget.RecyclerView;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import com.nostra13.universalimageloader.core.ImageLoader;
-import com.nostra13.universalimageloader.core.assist.FailReason;
-import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
-import org.fdroid.fdroid.views.AppDetailsActivity;
 import org.fdroid.fdroid.R;
 import org.fdroid.fdroid.Utils;
 import org.fdroid.fdroid.data.App;
-import org.fdroid.fdroid.views.apps.FeatureImage;
+import org.fdroid.fdroid.views.AppDetailsActivity;
 
 /**
  * The {@link AppCardController} can bind an app to several different layouts, as long as the layout
