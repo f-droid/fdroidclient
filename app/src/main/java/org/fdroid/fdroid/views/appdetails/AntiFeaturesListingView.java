f6b3c4bbe2d87705d2ce701b8b271d54df07c5ff
Dominik Schürmann <dominik@schuermann.eu>
2020-07-01 23:07:59 +0200

    Migrate AntiFeaturesListingView to AndroidX

--- app/src/main/java/org/fdroid/fdroid/views/appdetails/AntiFeaturesListingView.java
+++ app/src/main/java/org/fdroid/fdroid/views/appdetails/AntiFeaturesListingView.java
@@ -22,17 +22,18 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
-import android.support.annotation.DrawableRes;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import androidx.annotation.DrawableRes;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import org.fdroid.fdroid.R;
 import org.fdroid.fdroid.data.App;
