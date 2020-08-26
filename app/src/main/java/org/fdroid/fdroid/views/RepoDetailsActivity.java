b6aaa0b8551a1ab0022d17c194f44683b8e2628d
Isira Seneviratne <isirasen96@gmail.com>
2020-08-26 06:46:02 +0530

    Use view binding in RepoDetailsActivity.

--- app/src/main/java/org/fdroid/fdroid/views/RepoDetailsActivity.java
+++ app/src/main/java/org/fdroid/fdroid/views/RepoDetailsActivity.java
@@ -22,14 +22,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CompoundButton;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.NavUtils;
 import androidx.core.content.ContextCompat;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
@@ -45,6 +43,9 @@ import org.fdroid.fdroid.Utils;
 import org.fdroid.fdroid.data.Repo;
 import org.fdroid.fdroid.data.RepoProvider;
 import org.fdroid.fdroid.data.Schema.RepoTable;
+import org.fdroid.fdroid.databinding.ActivityRepoDetailsBinding;
+import org.fdroid.fdroid.databinding.LoginBinding;
+import org.fdroid.fdroid.databinding.RepoItemBinding;
 import org.fdroid.fdroid.qr.QrGenAsyncTask;
 
 import java.util.Arrays;
@@ -84,10 +85,11 @@ public class RepoDetailsActivity extends AppCompatActivity {
     };
     private Repo repo;
     private long repoId;
-    private View repoView;
     private String shareUrl;
 
     private MirrorAdapter adapterToNotify;
+
+    private ActivityRepoDetailsBinding repoDetailsBinding;
 
     /**
      * Help function to make switching between two view states easier.
@@ -103,32 +105,26 @@ public class RepoDetailsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         ((FDroidApp) getApplication()).applyTheme(this);
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_repo_details);
+        repoDetailsBinding = ActivityRepoDetailsBinding.inflate(getLayoutInflater());
+        setContentView(repoDetailsBinding.getRoot());
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setSupportActionBar(repoDetailsBinding.toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        repoView = findViewById(R.id.repo_view);
 
         repoId = getIntent().getLongExtra(ARG_REPO_ID, 0);
         repo = RepoProvider.Helper.findById(this, repoId);
 
-        TextView inputUrl = findViewById(R.id.input_repo_url);
-        inputUrl.setText(repo.address);
+        repoDetailsBinding.inputRepoUrl.setText(repo.address);
 
-        RecyclerView officialMirrorListView = findViewById(R.id.official_mirror_list);
-        officialMirrorListView.setLayoutManager(new LinearLayoutManager(this));
+        repoDetailsBinding.officialMirrorList.setLayoutManager(new LinearLayoutManager(this));
         adapterToNotify = new MirrorAdapter(repo, repo.mirrors);
-        officialMirrorListView.setAdapter(adapterToNotify);
+        repoDetailsBinding.officialMirrorList.setAdapter(adapterToNotify);
 
-        RecyclerView userMirrorListView = findViewById(R.id.user_mirror_list);
-        userMirrorListView.setLayoutManager(new LinearLayoutManager(this));
-        userMirrorListView.setAdapter(new MirrorAdapter(repo, repo.userMirrors));
+        repoDetailsBinding.userMirrorList.setLayoutManager(new LinearLayoutManager(this));
+        repoDetailsBinding.userMirrorList.setAdapter(new MirrorAdapter(repo, repo.userMirrors));
 
         if (repo.address.startsWith("content://")) {
             // no need to show a QR Code, it is not shareable
@@ -283,10 +279,9 @@ public class RepoDetailsActivity extends AppCompatActivity {
         }
     }
 
-    private void setupDescription(View parent, Repo repo) {
-
-        TextView descriptionLabel = (TextView) parent.findViewById(R.id.label_description);
-        TextView description = (TextView) parent.findViewById(R.id.text_description);
+    private void setupDescription(Repo repo) {
+        TextView descriptionLabel = repoDetailsBinding.repoView.findViewById(R.id.label_description);
+        TextView description = repoDetailsBinding.repoView.findViewById(R.id.text_description);
 
         if (TextUtils.isEmpty(repo.description)) {
             descriptionLabel.setVisibility(View.GONE);
@@ -299,9 +294,9 @@ public class RepoDetailsActivity extends AppCompatActivity {
         }
     }
 
-    private void setupRepoFingerprint(View parent, Repo repo) {
-        TextView repoFingerprintView = (TextView) parent.findViewById(R.id.text_repo_fingerprint);
-        TextView repoFingerprintDescView = (TextView) parent.findViewById(R.id.text_repo_fingerprint_description);
+    private void setupRepoFingerprint(Repo repo) {
+        TextView repoFingerprintView = repoDetailsBinding.repoView.findViewById(R.id.text_repo_fingerprint);
+        TextView repoFingerprintDescView = repoDetailsBinding.repoView.findViewById(R.id.text_repo_fingerprint_description);
 
         String repoFingerprint;
 
@@ -320,11 +315,10 @@ public class RepoDetailsActivity extends AppCompatActivity {
         repoFingerprintView.setText(repoFingerprint);
     }
 
-    private void setupCredentials(View parent, Repo repo) {
-
-        TextView usernameLabel = parent.findViewById(R.id.label_username);
-        TextView username = parent.findViewById(R.id.text_username);
-        Button changePassword = parent.findViewById(R.id.button_edit_credentials);
+    private void setupCredentials(Repo repo) {
+        TextView usernameLabel = repoDetailsBinding.repoView.findViewById(R.id.label_username);
+        TextView username = repoDetailsBinding.repoView.findViewById(R.id.text_username);
+        Button changePassword = repoDetailsBinding.repoView.findViewById(R.id.button_edit_credentials);
 
         if (TextUtils.isEmpty(repo.username)) {
             usernameLabel.setVisibility(View.GONE);
@@ -340,8 +334,8 @@ public class RepoDetailsActivity extends AppCompatActivity {
     }
 
     private void updateRepoView() {
-        TextView officialMirrorsLabel = repoView.findViewById(R.id.label_official_mirrors);
-        RecyclerView officialMirrorList = repoView.findViewById(R.id.official_mirror_list);
+        TextView officialMirrorsLabel = repoDetailsBinding.repoView.findViewById(R.id.label_official_mirrors);
+        RecyclerView officialMirrorList = repoDetailsBinding.repoView.findViewById(R.id.official_mirror_list);
         if ((repo.mirrors != null && repo.mirrors.length > 1)
                 || (repo.userMirrors != null && repo.userMirrors.length > 0)) {
             // don't show this if there is only the canonical URL available, and no other mirrors
@@ -352,8 +346,8 @@ public class RepoDetailsActivity extends AppCompatActivity {
             officialMirrorList.setVisibility(View.GONE);
         }
 
-        TextView userMirrorsLabel = repoView.findViewById(R.id.label_user_mirrors);
-        RecyclerView userMirrorList = repoView.findViewById(R.id.user_mirror_list);
+        TextView userMirrorsLabel = repoDetailsBinding.repoView.findViewById(R.id.label_user_mirrors);
+        RecyclerView userMirrorList = repoDetailsBinding.repoView.findViewById(R.id.user_mirror_list);
         if (repo.userMirrors != null && repo.userMirrors.length > 0) {
             userMirrorsLabel.setVisibility(View.VISIBLE);
             userMirrorList.setVisibility(View.VISIBLE);
@@ -363,29 +357,29 @@ public class RepoDetailsActivity extends AppCompatActivity {
         }
 
         if (repo.hasBeenUpdated()) {
-            updateViewForExistingRepo(repoView);
+            updateViewForExistingRepo();
         } else {
-            setMultipleViewVisibility(repoView, HIDE_IF_EXISTS, View.VISIBLE);
-            setMultipleViewVisibility(repoView, SHOW_IF_EXISTS, View.GONE);
+            setMultipleViewVisibility(repoDetailsBinding.repoView, HIDE_IF_EXISTS, View.VISIBLE);
+            setMultipleViewVisibility(repoDetailsBinding.repoView, SHOW_IF_EXISTS, View.GONE);
         }
     }
 
-    private void updateViewForExistingRepo(View repoView) {
-        setMultipleViewVisibility(repoView, SHOW_IF_EXISTS, View.VISIBLE);
-        setMultipleViewVisibility(repoView, HIDE_IF_EXISTS, View.GONE);
+    private void updateViewForExistingRepo() {
+        setMultipleViewVisibility(repoDetailsBinding.repoView, SHOW_IF_EXISTS, View.VISIBLE);
+        setMultipleViewVisibility(repoDetailsBinding.repoView, HIDE_IF_EXISTS, View.GONE);
 
-        TextView name = repoView.findViewById(R.id.text_repo_name);
-        TextView numApps = repoView.findViewById(R.id.text_num_apps);
-        TextView lastUpdated = repoView.findViewById(R.id.text_last_update);
+        TextView name = repoDetailsBinding.repoView.findViewById(R.id.text_repo_name);
+        TextView numApps = repoDetailsBinding.repoView.findViewById(R.id.text_num_apps);
+        TextView lastUpdated = repoDetailsBinding.repoView.findViewById(R.id.text_last_update);
 
         name.setText(repo.name);
 
         int appCount = RepoProvider.Helper.countAppsForRepo(this, repoId);
         numApps.setText(String.format(Locale.getDefault(), "%d", appCount));
 
-        setupDescription(repoView, repo);
-        setupRepoFingerprint(repoView, repo);
-        setupCredentials(repoView, repo);
+        setupDescription(repo);
+        setupRepoFingerprint(repo);
+        setupCredentials(repo);
 
         // Repos that existed before this feature was supported will have an
         // "Unknown" last update until next time they update...
@@ -420,13 +414,11 @@ public class RepoDetailsActivity extends AppCompatActivity {
     }
 
     public void showChangePasswordDialog(final View parentView) {
-        final View view = getLayoutInflater().inflate(R.layout.login, null);
-        final AlertDialog credentialsDialog = new AlertDialog.Builder(this).setView(view).create();
-        final EditText nameInput = view.findViewById(R.id.edit_name);
-        final EditText passwordInput = view.findViewById(R.id.edit_password);
+        final LoginBinding loginBinding = LoginBinding.inflate(getLayoutInflater());
+        final AlertDialog credentialsDialog = new AlertDialog.Builder(this).setView(loginBinding.getRoot()).create();
 
-        nameInput.setText(repo.username);
-        passwordInput.requestFocus();
+        loginBinding.editName.setText(repo.username);
+        loginBinding.editPassword.requestFocus();
 
         credentialsDialog.setTitle(R.string.repo_edit_credentials);
         credentialsDialog.setButton(DialogInterface.BUTTON_NEGATIVE,
@@ -443,12 +435,10 @@ public class RepoDetailsActivity extends AppCompatActivity {
                 new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-
-                        final String name = nameInput.getText().toString();
-                        final String password = passwordInput.getText().toString();
+                        final String name = loginBinding.editName.getText().toString();
+                        final String password = loginBinding.editPassword.getText().toString();
 
                         if (!TextUtils.isEmpty(name)) {
-
                             final ContentValues values = new ContentValues(2);
                             values.put(RepoTable.Cols.USERNAME, name);
                             values.put(RepoTable.Cols.PASSWORD, password);
@@ -458,9 +448,7 @@ public class RepoDetailsActivity extends AppCompatActivity {
                             updateRepoView();
 
                             dialog.dismiss();
-
                         } else {
-
                             Toast.makeText(RepoDetailsActivity.this, R.string.repo_error_empty_username,
                                     Toast.LENGTH_LONG).show();
                         }
@@ -475,11 +463,11 @@ public class RepoDetailsActivity extends AppCompatActivity {
         private final String[] mirrors;
 
         class MirrorViewHolder extends RecyclerView.ViewHolder {
-            View view;
+            RepoItemBinding repoItemBinding;
 
-            MirrorViewHolder(View view) {
-                super(view);
-                this.view = view;
+            MirrorViewHolder(RepoItemBinding repoItemBinding) {
+                super(repoItemBinding.getRoot());
+                this.repoItemBinding = repoItemBinding;
             }
         }
 
@@ -491,14 +479,14 @@ public class RepoDetailsActivity extends AppCompatActivity {
         @NonNull
         @Override
         public MirrorAdapter.MirrorViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.repo_item, parent, false);
-            return new MirrorViewHolder(itemView);
+            RepoItemBinding repoItemBinding = RepoItemBinding.inflate(LayoutInflater.from(parent.getContext()),
+                    parent, false);
+            return new MirrorViewHolder(repoItemBinding);
         }
 
         @Override
         public void onBindViewHolder(@NonNull MirrorViewHolder holder, final int position) {
-            TextView repoNameTextView = holder.view.findViewById(R.id.repo_name);
-            repoNameTextView.setText(mirrors[position]);
+            holder.repoItemBinding.repoName.setText(mirrors[position]);
 
             final String itemMirror = mirrors[position];
             boolean enabled = true;
@@ -510,9 +498,8 @@ public class RepoDetailsActivity extends AppCompatActivity {
                     }
                 }
             }
-            CompoundButton switchView = holder.view.findViewById(R.id.repo_switch);
-            switchView.setChecked(enabled);
-            switchView.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            holder.repoItemBinding.repoSwitch.setChecked(enabled);
+            holder.repoItemBinding.repoSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
                 @Override
                 public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                     HashSet<String> disabledMirrors;
@@ -548,11 +535,8 @@ public class RepoDetailsActivity extends AppCompatActivity {
                 }
             });
 
-            View repoUnverified = holder.view.findViewById(R.id.repo_unverified);
-            repoUnverified.setVisibility(View.GONE);
-
-            View repoUnsigned = holder.view.findViewById(R.id.repo_unsigned);
-            repoUnsigned.setVisibility(View.GONE);
+            holder.repoItemBinding.repoUnverified.setVisibility(View.GONE);
+            holder.repoItemBinding.repoUnsigned.setVisibility(View.GONE);
         }
 
         @Override
