eab5ef59b9f60dc24732ad978ac57a9f584a0bc2
Isira Seneviratne <isirasen96@gmail.com>
2020-10-25 15:17:36 +0530

    Use RxJava instead of AsyncTask to handle updates.

--- app/src/full/java/org/fdroid/fdroid/nearby/WifiStateChangeService.java
+++ app/src/full/java/org/fdroid/fdroid/nearby/WifiStateChangeService.java
@@ -13,6 +13,10 @@ import android.os.Build;
 import android.text.TextUtils;
 import android.util.Log;
 
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
 import org.apache.commons.net.util.SubnetUtils;
 import org.fdroid.fdroid.BuildConfig;
 import org.fdroid.fdroid.FDroidApp;
@@ -31,10 +35,8 @@ import java.security.cert.Certificate;
 import java.util.Enumeration;
 import java.util.Locale;
 
-import androidx.annotation.Nullable;
-import androidx.core.content.ContextCompat;
-import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import cc.mvdan.accesspoint.WifiApControl;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
 
 /**
  * Handle state changes to the device's wifi, storing the required bits.
@@ -70,6 +72,8 @@ public class WifiStateChangeService extends IntentService {
     private static int previousWifiState = Integer.MIN_VALUE;
     private static int wifiState;
 
+    private final CompositeDisposable compositeDisposable = new CompositeDisposable();
+
     public WifiStateChangeService() {
         super("WifiStateChangeService");
     }
@@ -80,6 +84,12 @@ public class WifiStateChangeService extends IntentService {
         }
         intent.setComponent(new ComponentName(context, WifiStateChangeService.class));
         context.startService(intent);
+    }
+
+    @Override
+    public void onDestroy() {
+        compositeDisposable.dispose();
+        super.onDestroy();
     }
 
     @Override
@@ -109,7 +119,7 @@ public class WifiStateChangeService extends IntentService {
             }
 
             if (Build.VERSION.SDK_INT < 21 && wifiState == WifiManager.WIFI_STATE_ENABLED) {
-                UpdateService.scheduleIfStillOnWifi(this);
+                compositeDisposable.add(UpdateService.scheduleIfStillOnWifi(this).subscribe());
             }
         }
     }
