f-droid/fdroidclient

commit b52c7ca39a485d552605028437dbebea4107152a

Remove duplicate entries from installed packages list

app/src/main/java/org/fdroid/fdroid/data/InstalledAppProviderService.java
@@ -23,10 +23,9 @@ import org.fdroid.fdroid.privileged.IPrivilegedService;
 
 import java.io.File;
 import java.io.FilenameFilter;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeSet;
 import java.util.concurrent.TimeUnit;
 
 import androidx.annotation.NonNull;
@@ -221,13 +220,9 @@ public class InstalledAppProviderService extends JobIntentService {
             packageInfoList = context.getPackageManager().getInstalledPackages(PackageManager.GET_SIGNATURES);
         }
         Map<String, Long> cachedInfo = InstalledAppProvider.Helper.lastUpdateTimes(context);
-        Collections.sort(packageInfoList, new Comparator<PackageInfo>() {
-            @Override
-            public int compare(PackageInfo o1, PackageInfo o2) {
-                return o1.packageName.compareTo(o2.packageName);
-            }
-        });
-        for (PackageInfo packageInfo : packageInfoList) {
+        TreeSet<PackageInfo> packageInfoSet = new TreeSet<>((o1, o2) -> o1.packageName.compareTo(o2.packageName));
+        packageInfoSet.addAll(packageInfoList);
+        for (PackageInfo packageInfo : packageInfoSet) {
             if (cachedInfo.containsKey(packageInfo.packageName)) {
                 if (packageInfo.lastUpdateTime < 1262300400000L // 2010-01-01 00:00
                         || packageInfo.lastUpdateTime > cachedInfo.get(packageInfo.packageName)) {

commit 2617a9ccea37fb16ff1b30fb0733a180d308f463

InstalledAppProvider: Fix Trichrome Library install
* The way Trichrome library (which is a static shared library)
  is implemented means that the PackageManager ends up with two
  entries for it, one for the system-installed version, and another
  for the upgrade installed on /data.
* The previous implementation which only relied on packageName would
  sometimes pick the older version, which meant that even after installing
  an update it'd still keep trying to re-install the same version over
  and over again since in F-Droid's internal database it still thought that
  an older version was installed.
* Always using the newer version / higher version code should hopefully fix this.

Fixes: b52c7ca39a485d552605028437dbebea4107152a
"Remove duplicate entries from installed packages list"

Issue: https://gitlab.com/fdroid/fdroidclient/-/issues/2184
Issue: calyxos#406

app/src/main/java/org/fdroid/fdroid/data/InstalledAppProviderService.java
@@ -23,6 +23,7 @@ import org.fdroid.fdroid.privileged.IPrivilegedService;
 
 import java.io.File;
 import java.io.FilenameFilter;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeSet;
@@ -220,12 +221,28 @@ public class InstalledAppProviderService extends JobIntentService {
         }
     }
 
+    private static class PackageInfoComparator implements Comparator<PackageInfo> {
+        @Override
+        public int compare(PackageInfo o1, PackageInfo o2) {
+            // There are two trichrome library entries in the list,
+            // one for each version. We only want the newest here.
+            String[] duplicateList = new String[]{"org.chromium.trichromelibrary"};
+            for (String dup : duplicateList) {
+                if (o1.packageName.contentEquals(dup)
+                        && o2.packageName.contentEquals(dup)) {
+                    return Integer.compare(o1.versionCode, o2.versionCode);
+                }
+            }
+            return o1.packageName.compareTo(o2.packageName);
+        }
+    }
+
     private static void compareToPackageManager(Context context, List<PackageInfo> packageInfoList) {
         if (packageInfoList == null || packageInfoList.isEmpty()) {
             packageInfoList = context.getPackageManager().getInstalledPackages(PackageManager.GET_SIGNATURES);
         }
         Map<String, Long> cachedInfo = InstalledAppProvider.Helper.lastUpdateTimes(context);
-        TreeSet<PackageInfo> packageInfoSet = new TreeSet<>((o1, o2) -> o1.packageName.compareTo(o2.packageName));
+        TreeSet<PackageInfo> packageInfoSet = new TreeSet<>(new PackageInfoComparator());
         packageInfoSet.addAll(packageInfoList);
         for (PackageInfo packageInfo : packageInfoSet) {
             if (cachedInfo.containsKey(packageInfo.packageName)) {