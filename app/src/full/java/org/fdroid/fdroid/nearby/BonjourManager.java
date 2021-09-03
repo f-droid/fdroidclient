f-droid/fdroidclient

commit f7c594b0450b6a761f4f9e0c600bfe035b88f459

show warning if VPN is active while trying to use nearby swap
refs #1386

app/src/full/java/org/fdroid/fdroid/nearby/BonjourManager.java
@@ -2,7 +2,11 @@ package org.fdroid.fdroid.nearby;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
 import android.net.wifi.WifiManager;
+import android.os.Build;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.Message;
@@ -48,6 +52,7 @@ public class BonjourManager {
     public static final int STATUS_STOPPED = 3;
     public static final int STATUS_VISIBLE = 4;
     public static final int STATUS_NOT_VISIBLE = 5;
+    public static final int STATUS_VPN_CONFLICT = 6;
     public static final int STATUS_ERROR = 0xffff;
 
     public static final String HTTP_SERVICE_TYPE = "_http._tcp.local.";
@@ -56,6 +61,7 @@ public class BonjourManager {
     private static final int STOP = 5709;
     private static final int VISIBLE = 4151873;
     private static final int NOT_VISIBLE = 144151873;
+    private static final int VPN_CONFLICT = 72346752;
 
     private static WeakReference<Context> context;
     private static Handler handler;
@@ -89,7 +95,9 @@ public class BonjourManager {
             Log.e(TAG, "handlerThread is stopped, not changing visibility!");
             return;
         }
-        if (visible) {
+        if (isVpnActive(context)) {
+            handler.sendEmptyMessage(VPN_CONFLICT);
+        } else if (visible) {
             handler.sendEmptyMessage(VISIBLE);
         } else {
             handler.sendEmptyMessage(NOT_VISIBLE);
@@ -160,6 +168,9 @@ public class BonjourManager {
                     case NOT_VISIBLE:
                         handleNotVisible();
                         break;
+                    case VPN_CONFLICT:
+                        handleVpnConflict();
+                        break;
                     case STOP:
                         handleStop();
                         break;
@@ -200,6 +211,10 @@ public class BonjourManager {
                     pairService = null;
                 }
                 sendBroadcast(STATUS_NOT_VISIBLE, null);
+            }
+
+            private void handleVpnConflict() {
+                sendBroadcast(STATUS_VPN_CONFLICT, null);
             }
 
             private void handleStop() {
@@ -283,5 +298,20 @@ public class BonjourManager {
         public void serviceResolved(ServiceEvent serviceEvent) {
             sendBroadcast(ACTION_FOUND, serviceEvent.getInfo());
         }
+    }
+
+    /**
+     * {@link ConnectivityManager#getActiveNetwork()} is only available
+     * starting on {@link Build.VERSION_CODES#M}, so for now, just return false
+     * if the device is too old.
+     */
+    public static boolean isVpnActive(Context context) {
+        if (Build.VERSION.SDK_INT < 23) {
+            return false;
+        }
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        Network activeNetwork = cm.getActiveNetwork();
+        NetworkCapabilities caps = cm.getNetworkCapabilities(activeNetwork);
+        return caps.hasTransport(NetworkCapabilities.TRANSPORT_VPN);
     }
 }

app/src/full/java/org/fdroid/fdroid/nearby/SwapWorkflowActivity.java
@@ -1039,7 +1039,6 @@ public class SwapWorkflowActivity extends AppCompatActivity {
                 return;
             }
             int status = intent.getIntExtra(BonjourManager.EXTRA_STATUS, -1);
-            Log.i(TAG, "BonjourManager.EXTRA_STATUS: " + status);
             switch (status) {
                 case BonjourManager.STATUS_STARTING:
                     textWifiVisible.setText(R.string.swap_setting_up_wifi);
@@ -1052,6 +1051,9 @@ public class SwapWorkflowActivity extends AppCompatActivity {
                     peopleNearbyText.setText(R.string.swap_scanning_for_peers);
                     peopleNearbyText.setVisibility(View.VISIBLE);
                     peopleNearbyProgress.setVisibility(View.VISIBLE);
+                    break;
+                case BonjourManager.STATUS_VPN_CONFLICT:
+                    textWifiVisible.setText(R.string.swap_wifi_vpn_conflict);
                     break;
                 case BonjourManager.STATUS_NOT_VISIBLE:
                     textWifiVisible.setText(R.string.swap_not_visible_wifi);

app/src/main/res/values/strings.xml
@@ -541,6 +541,7 @@ This often occurs with apps installed via Google Play or other sources, if they 
     <string name="swap_setting_up_wifi">Setting up Wi-Fi…</string>
     <string name="swap_stopping_wifi">Stopping Wi-Fi…</string>
     <string name="swap_not_visible_wifi">Not visible via Wi-Fi</string>
+    <string name="swap_wifi_vpn_conflict">Conflicts with active VPN!</string>
     <string name="swap_wifi_device_name">Device Name</string>
     <string name="swap_cant_find_peers">Can\'t find who you\'re looking for?</string>
     <!-- This is a button label, it must be the right size, or the layout gets messed up. It should be less than 15 characters. -->

commit 23ce28815f36c28bd35b09c72b7a7a9c6fbfc1a1

do not throw null pointer exception if trying Nearby without a net
Introduced in f7c594b0450b6a761f4f9e0c600bfe035b88f459 !1052

app/src/full/java/org/fdroid/fdroid/nearby/BonjourManager.java
@@ -312,6 +312,9 @@ public class BonjourManager {
         ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         Network activeNetwork = cm.getActiveNetwork();
         NetworkCapabilities caps = cm.getNetworkCapabilities(activeNetwork);
+        if (caps == null) {
+            return false;
+        }
         return caps.hasTransport(NetworkCapabilities.TRANSPORT_VPN);
     }
 }