5425b21eb27b87d921501c168396ad7e0d384a57
Michael Pöhn <michael@poehn.at>
2023-11-08 14:52:28 +0100

    🤌 fix linter nit-picks on IPreferencesIpfs

--- app/src/main/java/org/fdroid/fdroid/IPreferencesIpfs.java
+++ app/src/main/java/org/fdroid/fdroid/IPreferencesIpfs.java
@@ -1,24 +1,23 @@
 package org.fdroid.fdroid;
 
-
 import java.util.Set;
 
 /**
  * Interface for accessing IPFS related preferences.
- *
+ * <p>
  * Splitting this into a separate interfaces allows for easily injecting data into Compose UI
  * previews. (and maybe unit-tests too)
  */
 public interface IPreferencesIpfs {
-    public boolean isIpfsEnabled();
+    boolean isIpfsEnabled();
 
-    public void setIpfsEnabled(boolean enabled);
+    void setIpfsEnabled(boolean enabled);
 
-    public Set<String> getIpfsGwDisabledDefaults();
+    Set<String> getIpfsGwDisabledDefaults();
 
-    public void setIpfsGwDisabledDefaults(Set<String> selectedSet);
+    void setIpfsGwDisabledDefaults(Set<String> selectedSet);
 
-    public Set<String> getIpfsGwUserList();
+    Set<String> getIpfsGwUserList();
 
-    public void setIpfsGwUserList(Set<String> selectedSet);
+    void setIpfsGwUserList(Set<String> selectedSet);
 }
