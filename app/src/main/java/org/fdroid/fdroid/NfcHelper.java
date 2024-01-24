ff5d26e79fa31b0fcb169db15d2058bc849075b5
Torsten Grote <t@grobox.de>
2024-01-24 10:24:02 -0300

    Make pmd happy

--- app/src/main/java/org/fdroid/fdroid/NfcHelper.java
+++ app/src/main/java/org/fdroid/fdroid/NfcHelper.java
@@ -1,19 +1,11 @@
 package org.fdroid.fdroid;
 
-import android.content.Context;
 import android.net.Uri;
-import android.nfc.NfcAdapter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 @Deprecated
 public class NfcHelper {
-
-    private static final String TAG = "NfcHelper";
-
-    private static NfcAdapter getAdapter(Context context) {
-        return NfcAdapter.getDefaultAdapter(context.getApplicationContext());
-    }
 
     public static boolean setPushMessage(AppCompatActivity activity, Uri toShare) {
         // removed in Android 14: https://www.xda-developers.com/android-beam-permanent-removal-android-14/
