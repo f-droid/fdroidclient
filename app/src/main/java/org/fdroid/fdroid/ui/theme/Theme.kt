29cdce0cf9cb3b4ef6e1bbbf34b7b97dfd35d2b1
proletarius101 <proletarius101@protonmail.com>
2025-02-05 06:26:58 -0800

    fix: miss surfaceContainer colors for Compose

--- app/src/main/java/org/fdroid/fdroid/ui/theme/Theme.kt
+++ app/src/main/java/org/fdroid/fdroid/ui/theme/Theme.kt
@@ -42,6 +42,13 @@ private val lightScheme = lightColorScheme(
     inverseSurface = inverseSurfaceLight,
     inverseOnSurface = inverseOnSurfaceLight,
     inversePrimary = inversePrimaryLight,
+    surfaceDim = surfaceDimLight,
+    surfaceBright = surfaceBrightLight,
+    surfaceContainerLowest = surfaceContainerLowestLight,
+    surfaceContainerLow = surfaceContainerLowLight,
+    surfaceContainer = surfaceContainerLight,
+    surfaceContainerHigh = surfaceContainerHighLight,
+    surfaceContainerHighest = surfaceContainerHighestLight,
 )
 
 private val darkScheme = darkColorScheme(
