36fcbbf015f156708f9f5c73f9cba851573c6074
Torsten Grote <t@grobox.de>
2026-02-25 10:16:56 -0300

    Release 2.0-alpha4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "org.fdroid"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2000003
-        versionName = "2.0-alpha3"
+        versionCode = 2000004
+        versionName = "2.0-alpha4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
