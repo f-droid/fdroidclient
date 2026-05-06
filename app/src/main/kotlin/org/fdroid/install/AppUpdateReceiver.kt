4da7bafbee7ddfd5dc08a11b5af705ea1ee2d784
Torsten Grote <t@grobox.de>
2026-05-06 10:18:46 -0300

    Show notification after self-update
    because we aren't allowed to launch the app. So the user can at least re-launch us easily.

--- app/src/main/kotlin/org/fdroid/install/AppUpdateReceiver.kt
+++ app/src/main/kotlin/org/fdroid/install/AppUpdateReceiver.kt
@@ -5,14 +5,19 @@ import android.content.Context
 import android.content.Intent
 import android.content.Intent.ACTION_MY_PACKAGE_REPLACED
 import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
-import androidx.annotation.RequiresApi
+import android.os.Build.VERSION.SDK_INT
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 import mu.KotlinLogging
+import org.fdroid.NotificationManager
 
+@AndroidEntryPoint
 class AppUpdateReceiver : BroadcastReceiver() {
 
   private val log = KotlinLogging.logger {}
 
-  @RequiresApi(35)
+  @Inject lateinit var notificationManager: NotificationManager
+
   override fun onReceive(context: Context, intent: Intent) {
     if (intent.action != ACTION_MY_PACKAGE_REPLACED) {
       log.warn { "Unknown action: ${intent.action}" }
@@ -23,12 +28,17 @@ class AppUpdateReceiver : BroadcastReceiver() {
       context.packageManager.getLaunchIntentForPackage(context.packageName)?.apply {
         addFlags(FLAG_ACTIVITY_NEW_TASK)
       }
-    if (intent != null) {
+    if (intent == null) {
+      log.error { "Could not get launch intent for ourselves" }
+    } else {
       try {
         context.startActivity(intent)
       } catch (e: Exception) {
         log.error(e) { "Failed to start activity after update" }
       }
     }
+    // show notification on Android 10+, because we aren't allowed to start activity from background
+    // see: https://developer.android.com/guide/components/activities/background-starts
+    if (SDK_INT >= 29) notificationManager.showSelfUpdateNotification()
   }
 }
