b3aea44cf0e2d0c5bcbec63bf1f57bfb21cc35d8
Torsten Grote <t@grobox.de>
2026-06-15 11:41:36 -0300

    Also clip app icons in app lists
    so they use rounded corners there as well

--- app/src/main/kotlin/org/fdroid/ui/lists/AppListRow.kt
+++ app/src/main/kotlin/org/fdroid/ui/lists/AppListRow.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.semantics.hideFromAccessibility
@@ -32,7 +33,10 @@ fun AppListRow(item: AppListItem, isSelected: Boolean, modifier: Modifier = Modi
           model = item.iconModel,
           error = painterResource(R.drawable.ic_repo_app_default),
           contentDescription = null,
-          modifier = Modifier.size(48.dp).semantics { hideFromAccessibility() },
+          modifier =
+            Modifier.size(48.dp).clip(MaterialTheme.shapes.medium).semantics {
+              hideFromAccessibility()
+            },
         )
       }
     },
