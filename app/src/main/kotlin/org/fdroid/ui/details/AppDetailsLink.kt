8d09dc01c31317bd3b9e415403c2a15435adb72c
Michael Pöhn <michael@poehn.at>
2026-07-09 13:25:11 +0200

    🖼️ opencollective/liberapay/bitcoin/litecoin icons
    This adds colorful icons to the donation section on the AppDetails
    composable.

--- app/src/main/kotlin/org/fdroid/ui/details/AppDetailsLink.kt
+++ app/src/main/kotlin/org/fdroid/ui/details/AppDetailsLink.kt
@@ -6,13 +6,16 @@ import androidx.compose.foundation.layout.Arrangement.spacedBy
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.platform.ClipEntry
 import androidx.compose.ui.platform.LocalClipboard
@@ -25,7 +28,13 @@ import org.fdroid.R
 import org.fdroid.ui.utils.openUriSafe
 
 @Composable
-fun AppDetailsLink(icon: ImageVector, title: String, url: String, modifier: Modifier = Modifier) {
+fun AppDetailsLink(
+  icon: Painter,
+  title: String,
+  url: String,
+  disableTinting: Boolean = false,
+  modifier: Modifier = Modifier)
+{
   val uriHandler = LocalUriHandler.current
   val haptics = LocalHapticFeedback.current
   val clipboardManager = LocalClipboard.current
@@ -47,7 +56,13 @@ fun AppDetailsLink(icon: ImageVector, title: String, url: String, modifier: Modi
           onLongClickLabel = stringResource(R.string.copy_link),
         ),
   ) {
-    Icon(icon, null)
+    Icon(
+      icon,
+      null,
+      // don't tint colorful icons (e.g. for donation options)
+      tint = if (disableTinting) Color.Unspecified else LocalContentColor.current,
+      modifier = Modifier.size(24.dp)
+    )
     Text(title)
   }
 }
